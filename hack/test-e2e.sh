c32880ee19cad1662acfac530a858605805ba1d9
Eric Wolinetz <ewolinet@redhat.com>
2018-12-14 16:34:25 -0600

    Cleaning up e2e tests and removing dependency on operator-sdk to test

--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -39,8 +39,11 @@ oc adm policy add-scc-to-user privileged -z rsyslog -n openshift-logging
 # allows rsyslog to query k8s api for all namespace/pod info
 oc adm policy add-cluster-role-to-user cluster-reader -z rsyslog -n openshift-logging
 
-operator-sdk test local \
-  --namespace openshift-logging \
-  ./test/e2e \
-  --namespaced-manifest ${manifest} \
-  --global-manifest  ${global_manifest}
+TEST_NAMESPACE=openshift-logging go test ./test/e2e/... \
+  -root=$(pwd) \
+  -kubeconfig=$HOME/.kube/config \
+  -globalMan ${global_manifest} \
+  -namespacedMan ${manifest} \
+  -v \
+  -parallel=1 \
+  -singleNamespace

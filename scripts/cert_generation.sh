43974cdbc949491cfef8d47e027c48b7087860cd
Eric Wolinetz <ewolinet@redhat.com>
2019-03-08 16:43:57 -0600

    expanding the cert dns entry for the es svcs

--- scripts/cert_generation.sh
+++ scripts/cert_generation.sh
@@ -262,5 +262,5 @@ generate_certs 'system.admin'
 
 # TODO: get es SAN DNS, IP values from es service names
 generate_certs 'kibana-internal' "$(generate_extensions false false kibana)"
-generate_certs 'elasticsearch' "$(generate_extensions true true elasticsearch)"
-generate_certs 'logging-es' "$(generate_extensions false true elasticsearch{,-cluster}{,.${NAMESPACE}.svc.cluster.local})"
+generate_certs 'elasticsearch' "$(generate_extensions true true elasticsearch{,-cluster}{,.${NAMESPACE}.svc}{,.cluster.local})"
+generate_certs 'logging-es' "$(generate_extensions false true elasticsearch{,.${NAMESPACE}.svc}{,.cluster.local})"

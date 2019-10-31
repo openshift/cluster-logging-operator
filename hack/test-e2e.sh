2081f4c234ec3d66670683663f411e3e8dbd67e5
Jeff Cantrill <jcantril@redhat.com>
2019-10-31 10:51:05 -0400

    enable upgrade test

--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -8,14 +8,13 @@ for test in $( find "${current_dir}/testing" -type f -name 'test-*.sh' | sort); 
 	os::log::info "======================================"
 	os::log::info "running e2e $test "
 	os::log::info "======================================"
-	if [ "$(basename $test)" == "test-020-olm-upgrade.sh" ] ; then
-		os::log::warning "Intentionally skipping $test"
-		continue
-	fi
 	if "${test}" ; then
 		os::log::info "e2e $test succeeded at $( date )"
 	else
-		os::log::warning "e2e $test failed at $( date )"
+
+		os::log::error "============= FAILED FAILED ============= "
+		os::log::error "e2e $test failed at $( date )"
+		os::log::error "============= FAILED FAILED ============= "
 		failed="true"
 	fi
 done

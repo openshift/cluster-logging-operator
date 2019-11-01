44f603f7ab78cbba062fa2b8d4ebed486c308f2c
Jeff Cantrill <jcantril@redhat.com>
2019-10-31 23:54:20 -0400

    LOG-478. Add promtail collector

--- pkg/controller/add_controllers.go
+++ pkg/controller/add_controllers.go
@@ -2,10 +2,11 @@ package controller
 
 import (
 	"github.com/openshift/cluster-logging-operator/pkg/controller/clusterlogging"
+	"github.com/openshift/cluster-logging-operator/pkg/controller/collector"
 	"github.com/openshift/cluster-logging-operator/pkg/controller/forwarding"
 )
 
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	AddToManagerFuncs = append(AddToManagerFuncs, clusterlogging.Add, forwarding.Add)
+	AddToManagerFuncs = append(AddToManagerFuncs, clusterlogging.Add, forwarding.Add, collector.Add)
 }

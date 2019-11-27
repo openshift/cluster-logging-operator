0176302c20461489de5d7d23447c6c38114b3376
Jeff Cantrill <jcantril@redhat.com>
2019-11-27 10:08:45 -0500

    bump manifest to 4.4

--- vendor/github.com/openshift/elasticsearch-operator/hack/deploy.sh
+++ vendor/github.com/openshift/elasticsearch-operator/hack/deploy.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 source "$(dirname $0)/common"
 
-if [ $REMOTE_REGISTRY = false ] ; then
-    oc create -n ${NAMESPACE} -f manifests/05-deployment.yaml
-else
-    if [ -n "${IMAGE_OVERRIDE:-}" ] ; then
-        replace_image() {
-            sed -e "s, image:.*\$, image: ${IMAGE_OVERRIDE},"
-        }
-    else
-        replace_image() {
-            sed -e "s,${IMAGE_TAG},${registry_host}:5000/${image_tag},"
-        }
-    fi
-    image_tag=$( echo "$IMAGE_TAG" | sed -e 's,quay.io/,,' )
-    cat manifests/05-deployment.yaml | \
-        replace_image | \
-	    oc create -n ${NAMESPACE} -f -
-fi
+IMAGE_ELASTICSEARCH_OPERATOR=image-registry.openshift-image-registry.svc:5000/openshift/origin-elasticsearch-operator:latest \
+deploy_elasticsearch_operator

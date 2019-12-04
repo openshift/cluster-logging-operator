bc1d3b8ae646784a9a380d72334d9d098a7741b4
Jeff Cantrill <jcantril@redhat.com>
2019-12-04 16:42:25 -0500

    Allow logforwarding tests to cleanup from go invocation

--- test/helpers/elasticsearch.go
+++ test/helpers/elasticsearch.go
@@ -91,6 +91,10 @@ func (es *ElasticLogStore) HasInfraStructureLogs(timeToWait time.Duration) (bool
 	err := wait.Poll(defaultRetryInterval, timeToWait, func() (done bool, err error) {
 		indices, err := es.Indices()
 		if err != nil {
+			logger.Errorf("Error retrieving indices from elasticsearch %v", err)
+			if strings.Contains(err.Error(), "failed to get pod") {
+				return false, nil
+			}
 			return false, err
 		}
 		return indices.HasInfraStructureLogs(), nil
@@ -102,6 +106,10 @@ func (es *ElasticLogStore) HasApplicationLogs(timeToWait time.Duration) (bool, e
 	err := wait.Poll(defaultRetryInterval, timeToWait, func() (done bool, err error) {
 		indices, err := es.Indices()
 		if err != nil {
+			logger.Errorf("Error retrieving indices from elasticsearch %v", err)
+			if strings.Contains(err.Error(), "failed to get pod") {
+				return false, nil
+			}
 			return false, err
 		}
 		return indices.HasApplicationLogs(), nil

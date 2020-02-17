180bb9ad82ba0cba58a8654c394cf89297aa851a
Eric Wolinetz <ewolinet@redhat.com>
2020-02-17 12:12:00 -0600

    Updating to require trustedcabundle cm and restrict proxy triggers to be update

--- pkg/controller/add_controllers.go
+++ pkg/controller/add_controllers.go
@@ -6,9 +6,16 @@ import (
 	"github.com/openshift/cluster-logging-operator/pkg/controller/forwarding"
 	"github.com/openshift/cluster-logging-operator/pkg/controller/kibanasecret"
 	"github.com/openshift/cluster-logging-operator/pkg/controller/proxyconfig"
+	"github.com/openshift/cluster-logging-operator/pkg/controller/trustedcabundle"
 )
 
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	AddToManagerFuncs = append(AddToManagerFuncs, clusterlogging.Add, forwarding.Add, collector.Add, proxyconfig.Add, kibanasecret.Add)
+	AddToManagerFuncs = append(AddToManagerFuncs,
+		clusterlogging.Add,
+		forwarding.Add,
+		collector.Add,
+		proxyconfig.Add,
+		kibanasecret.Add,
+		trustedcabundle.Add)
 }

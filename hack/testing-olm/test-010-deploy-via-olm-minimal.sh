579acd2afce176fbd28b8118f16c2347ce916085
Ben Parees <bparees@redhat.com>
2020-03-20 16:47:26 -0400

    add olm based deploy scripts for cluster logging operator
    also adds e2e based on olm deployment
    
    also leverages elasticsearch's olm deployment flow

---
+++ hack/testing-olm/test-010-deploy-via-olm-minimal.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Given an OLM manifest, verify a green field deployment
+# of cluster logging by asserting CLO creates the resources
+# that begets the operands that make up logging.
+
+set -eou pipefail
+
+source "$(dirname "${BASH_SOURCE[0]}" )/../lib/init.sh"
+source "$(dirname $0)/assertions"
+
+os::test::junit::declare_suite_start "${BASH_SOURCE[0]}"
+
+ARTIFACT_DIR=${ARTIFACT_DIR:-"$(pwd)/_output"}
+test_artifactdir="${ARTIFACT_DIR}/$(basename ${BASH_SOURCE[0]})"
+if [ ! -d $test_artifactdir ] ; then
+  mkdir -p $test_artifactdir
+fi
+
+export CLUSTER_LOGGING_OPERATOR_NAMESPACE="openshift-logging"
+repo_dir="$(dirname $0)/../.."
+manifest=${repo_dir}/manifests
+version=$(basename $(find $manifest -type d | sort -r | head -n 1))
+
+cleanup(){
+  local return_code="$?"
+  set +e
+  gather_logging_resources ${CLUSTER_LOGGING_OPERATOR_NAMESPACE} $test_artifactdir
+
+  ${repo_dir}/olm_deploy/scripts/operator-uninstall.sh
+  ${repo_dir}/olm_deploy/scripts/catalog-uninstall.sh
+  
+  os::cleanup::all "${return_code}"
+  
+  set -e
+  exit ${return_code}
+}
+trap cleanup exit
+
+KUBECONFIG=${KUBECONFIG:-$HOME/.kube/config}
+
+${repo_dir}/olm_deploy/scripts/catalog-deploy.sh
+${repo_dir}/olm_deploy/scripts/operator-install.sh
+
+TIMEOUT_MIN=$((2 * $minute))
+
+##verify metrics rbac
+# extra resources not support for ConfigMap based catelogs for now.
+#os::cmd::expect_success "oc get clusterrole clusterlogging-collector-metrics"
+#os::cmd::expect_success "oc get clusterrolebinding clusterlogging-collector-metrics"
+
+# wait for operator to be ready
+os::cmd::try_until_text "oc -n $NAMESPACE get deployment cluster-logging-operator -o jsonpath={.status.availableReplicas} --ignore-not-found" "1" ${TIMEOUT_MIN}
+
+# deploy cluster logging
+os::cmd::expect_success "oc -n $NAMESPACE create -f ${repo_dir}/hack/cr.yaml"
+
+# assert deployment
+assert_resources_exist
---
+++ hack/testing-olm/test-999-fluentd-prometheus-metrics.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -euo pipefail
+
+repo_dir=${repo_dir:-$(dirname $0)/../..}
+source "$repo_dir/hack/testing-olm/utils"
+
+ARTIFACT_DIR=${ARTIFACT_DIR:-$repo_dir/_output}
+LOGGING_NS=${LOGGING_NS:-openshift-logging}
+
+test_name=test-999-fluentd-prometheus-metrics
+
+cleanup() {
+  local return_code="$?"
+  set +e
+  mkdir -p $ARTIFACT_DIR/$test_name
+  oc -n $LOGGING_NS get configmap fluentd -o jsonpath={.data} > $ARTIFACT_DIR/$test_name/fluent-configmap.log
+	get_all_logging_pod_logs $ARTIFACT_DIR/$test_name
+
+	if [ "${DO_CLEANUP:-true}" == "true" ] ; then
+    ${repo_dir}/olm_deploy/scripts/operator-uninstall.sh
+    ${repo_dir}/olm_deploy/scripts/catalog-uninstall.sh
+	fi
+
+  set -e
+  exit $return_code
+}
+trap "cleanup" EXIT
+
+if [ "${DO_SETUP:-true}" == "true" ] ; then
+	log::info "Deploying cluster-logging-operator"
+  ${repo_dir}/olm_deploy/scripts/catalog-deploy.sh
+  ${repo_dir}/olm_deploy/scripts/operator-install.sh
+
+	cat <<EOL | oc -n ${LOGGING_NS} create -f -
+apiVersion: "logging.openshift.io/v1"
+kind: "ClusterLogging"
+metadata:
+  name: "instance"
+spec:
+  managementState: "Managed"
+  logStore:
+    type: "elasticsearch"
+    elasticsearch:
+      nodeCount: 1
+      redundancyPolicy: "ZeroRedundancy"
+      resources:
+        request:
+          memory: 1Gi
+          cpu: 100m
+  collection:
+    logs:
+      type: "fluentd"
+      fluentd: {}
+EOL
+fi
+try_until_text "oc -n ${LOGGING_NS} get ds fluentd -o jsonpath={.metadata.name} --ignore-not-found" "fluentd" "$((1 * $minute))"
+desired=$(oc -n ${LOGGING_NS} get ds fluentd  -o jsonpath={.status.desiredNumberScheduled})
+log::info "Waiting for ${desired} fluent pods to be available...."
+try_until_text "oc -n ${LOGGING_NS} get ds fluentd -o jsonpath={.status.numberReady}" "$desired" "$((2 * $minute))"
+
+fpod=$(oc -n $LOGGING_NS get pod -l component=fluentd -o jsonpath={.items[0].metadata.name} --ignore-not-found)
+
+log::info "Checking metrics using 'localhost'..."
+try_until_success "oc -n $LOGGING_NS exec $fpod -- curl -ks https://localhost:24231/metrics" "$((2 * $minute))"
+
+log::info "Checking metrics using fluent service address..."
+try_until_success "oc -n $LOGGING_NS exec $fpod -- curl -ks https://fluentd.openshift-logging.svc:24231/metrics" "$((2 * $minute))"
+
+fpod_ip="$(oc -n $LOGGING_NS get pod ${fpod} -o jsonpath='{.status.podIP}')"
+log::info "Checking metrics using fluent podIP..."
+try_until_success "oc -n $LOGGING_NS exec $fpod -- curl -ks https://${fpod_ip}:24231/metrics" "$((2 * $minute))"
+
+oc -n $LOGGING_NS exec $fpod -- curl -ks https://${fpod_ip}:24231/metrics >> $ARTIFACT_DIR/${fpod}-metrics-scrape 2>&1 || :

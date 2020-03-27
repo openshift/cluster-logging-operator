5aa96ae49bb96a8a2543d8ea929572da35efe94e
Jeff Cantrill <jcantril@redhat.com>
2020-03-27 15:13:19 -0400

    remove dots in index management types

--- pkg/generators/forwarding/fluentd/templates.go
+++ pkg/generators/forwarding/fluentd/templates.go
@@ -246,7 +246,7 @@ const fluentConfTemplate = `{{- define "fluentConf" }}
 			enabled "#{ENV['ENABLE_ES_INDEX_NAME'] || 'true'}"
 			tag "linux-audit.log** k8s-audit.log** openshift-audit.log**"
 			name_type static
-			static_index_name audit-infra-write
+			static_index_name audit-write
 		</elasticsearch_index_name>
 		<elasticsearch_index_name>
 			enabled "#{ENV['ENABLE_ES_INDEX_NAME'] || 'true'}"

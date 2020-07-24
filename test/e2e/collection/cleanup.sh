32b3632d23042aeb8630a228649aacb7c2511984
Periklis Tsirakidis <periklis@redhat.com>
2020-07-24 14:33:14 +0200

    Improve E2E cleanup scripts

--- test/e2e/collection/cleanup.sh
+++ test/e2e/collection/cleanup.sh
@@ -3,9 +3,9 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../../
 artifact_dir=$1
 GENERATOR_NS=$2
 runtime=$(date +%s)
-mkdir $artifact_dir/$runtime ||:
-gather_logging_resources "openshift-logging" $artifact_dir $runtime
+mkdir -p "$artifact_dir/$runtime" ||:
+gather_logging_resources "openshift-logging" "$artifact_dir" "$runtime"
 
-oc -n $GENERATOR_NS describe deployment/log-generator  > $artifact_dir/$runtime/log-generator.describe||:
-oc -n $GENERATOR_NS logs deployment/log-generator  > $artifact_dir/$runtime/log-generator.logs||:
-oc -n $GENERATOR_NS get deployment/log-generator -o yaml > $artifact_dir/$runtime/log-generator.deployment.yaml||:
+oc -n "$GENERATOR_NS" describe deployment/log-generator  > "$artifact_dir/$runtime/log-generator.describe" ||:
+oc -n "$GENERATOR_NS" logs deployment/log-generator  > "$artifact_dir/$runtime/log-generator.logs" ||:
+oc -n "$GENERATOR_NS" get deployment/log-generator -o yaml > "$artifact_dir/$runtime/log-generator.deployment.yaml" ||:

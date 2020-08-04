edcc7a5e1e31ef2e1f3fe5af30eb9abbfa6d27a9
Periklis Tsirakidis <periklis@redhat.com>
2020-08-04 16:20:54 +0200

    Add CRD printer columns and categories

--- pkg/apis/logging/v1/cluster_log_forwarder_types.go
+++ pkg/apis/logging/v1/cluster_log_forwarder_types.go
@@ -9,7 +9,7 @@ const ClusterLogForwarderKind = "ClusterLogForwarder"
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:shortName=clf,scope=Cluster
+// +kubebuilder:resource:categories=logging,shortName=clf,scope=Cluster
 
 // ClusterLogForwarder is an API to configure forwarding logs.
 //

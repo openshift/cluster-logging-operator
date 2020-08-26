39653fef73d657a8be1a2890bc78d9a0b0f390b6
Jeff Cantrill <jcantril@redhat.com>
2020-08-26 15:50:48 -0400

    LOG-720: Collect test artifacts using logging must-gather

--- hack/testing-olm/test-999-fluentd-prometheus-metrics.sh
+++ hack/testing-olm/test-999-fluentd-prometheus-metrics.sh
@@ -21,8 +21,7 @@ cleanup() {
 
   set +e
   mkdir -p $ARTIFACT_DIR/$test_name
-  oc -n $LOGGING_NS get configmap fluentd -o jsonpath={.data} > $ARTIFACT_DIR/$test_name/fluent-configmap.log
-  get_all_logging_pod_logs $ARTIFACT_DIR/$test_name
+  gather_logging_resources ${CLUSTER_LOGGING_OPERATOR_NAMESPACE} $ARTIFACT_DIR/$test_name
 
   if [ "${DO_CLEANUP:-true}" == "true" ] ; then
       ${repo_dir}/olm_deploy/scripts/operator-uninstall.sh

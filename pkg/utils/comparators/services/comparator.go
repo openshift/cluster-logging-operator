efac0a6b86f797a6e61ca9df834c83556730285e
Vitalii Parfonov <vparfono@redhat.com>
2020-10-20 11:31:39 +0300

    LOG-745: Consistent logging: Remove pkg/logger/logger.go
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- pkg/utils/comparators/services/comparator.go
+++ pkg/utils/comparators/services/comparator.go
@@ -1,23 +1,22 @@
 package services
 
 import (
-	"reflect"
-
-	"github.com/openshift/cluster-logging-operator/pkg/logger"
+	"github.com/ViaQ/logerr/log"
 	"github.com/openshift/cluster-logging-operator/pkg/utils"
 	v1 "k8s.io/api/core/v1"
+	"reflect"
 )
 
 //AreSame compares for equality and return true equal otherwise false
 func AreSame(current *v1.Service, desired *v1.Service) bool {
-	logger.Tracef("Comparing Services current %v to desired %v", current, desired)
+	log.V(3).Info("Comparing Services current to desired", "current", current, "desired", desired)
 
 	if !utils.AreMapsSame(current.ObjectMeta.Labels, desired.ObjectMeta.Labels) {
-		logger.Debugf("Service %q label change", current.Name)
+		log.V(3).Info("Service label change", "current name", current.Name)
 		return false
 	}
 	if !utils.AreMapsSame(current.Spec.Selector, desired.Spec.Selector) {
-		logger.Debugf("Service %q Selector change", current.Name)
+		log.V(3).Info("Service Selector change", "current name", current.Name)
 		return false
 	}
 	if len(current.Spec.Ports) != len(desired.Spec.Ports) {

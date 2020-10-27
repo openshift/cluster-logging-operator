23d8a16a474ba24ea30a2baff26fff525f24368e
Vitalii Parfonov <vparfono@redhat.com>
2020-10-28 00:09:21 +0200

    Try to avoid use Plugin Helper:Record Accessor
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- pkg/generators/forwarding/fluentd/fluent_conf_test.go
+++ pkg/generators/forwarding/fluentd/fluent_conf_test.go
@@ -399,7 +399,7 @@ var _ = Describe("Generating fluentd config", func() {
       <record>
         kubernetes ${!record['kubernetes'].nil? ? record['kubernetes'].merge({"flat_labels": (record['kubernetes']['labels']||{}).map{|k,v| "#{k}=#{v}"}}) : {} }
       </record>
-      remove_keys $.kubernetes.labels
+      remove_keys record[kubernetes][labels]
     </filter>
   
     # Relabel specific source tags to specific intermediary labels for copy processing
@@ -868,7 +868,7 @@ var _ = Describe("Generating fluentd config", func() {
 					<record>
 						kubernetes ${!record['kubernetes'].nil? ? record['kubernetes'].merge({"flat_labels": (record['kubernetes']['labels']||{}).map{|k,v| "#{k}=#{v}"}}) : {} }
 					</record>
-					remove_keys $.kubernetes.labels
+					remove_keys record[kubernetes][labels]
 				</filter>
 
 
@@ -1302,7 +1302,7 @@ var _ = Describe("Generating fluentd config", func() {
 					<record>
 						kubernetes ${!record['kubernetes'].nil? ? record['kubernetes'].merge({"flat_labels": (record['kubernetes']['labels']||{}).map{|k,v| "#{k}=#{v}"}}) : {} }
 					</record>
-					remove_keys $.kubernetes.labels
+					remove_keys record[kubernetes][labels]
 				</filter>
 
 				# Relabel specific source tags to specific intermediary labels for copy processing
@@ -2122,7 +2122,7 @@ var _ = Describe("Generating fluentd config", func() {
         <record>
           kubernetes ${!record['kubernetes'].nil? ? record['kubernetes'].merge({"flat_labels": (record['kubernetes']['labels']||{}).map{|k,v| "#{k}=#{v}"}}) : {} }
         </record>
-        remove_keys $.kubernetes.labels
+        remove_keys record[kubernetes][labels]
       </filter>
     
       # Relabel specific source tags to specific intermediary labels for copy processing

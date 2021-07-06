2242ed7682eecd1e7e5b19cc56f136dae587835f
Vimal Kumar <vimal78@gmail.com>
2021-07-06 11:52:55 +0530

    Revert "Fix hack/registry.sh for CI"
    This reverts commit b565cc2a1f1a2783b06f80c3d81aaba575a3564c.

--- hack/registry.sh
+++ hack/registry.sh
@@ -3,23 +3,20 @@
 
 set -e
 
-# This flag is set during CI runs.
-if [[ "${OPENSHIFT_CI:-}" == "true" ]]; then
-    oc registry info $* && exit 0	# Already works
+oc registry info $* && exit 0	# Already works
 
-    if [ "$1" = --public ]; then
-        echo "activate public registry route and log in, may take several retries" 1>&2
-        oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge 1>&2
-        RETRY=0
-        until [ $(( RETRY++ )) -eq 50 ]; do
-        REGISTRY=$(oc registry info --public) &&
-            test -n "$REGISTRY" &&
-            oc registry login --insecure --registry $REGISTRY 1>&2 &&
-            echo $REGISTRY &&
-            exit 0
-        echo "retry registry login.." 1>&2
-        sleep 6
-        done
-    fi
+if [ "$1" = --public ]; then
+    echo "activate public registry route and log in, may take several retries" 1>&2
+    oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge 1>&2
+    RETRY=0
+    until [ $(( RETRY++ )) -eq 50 ]; do
+	REGISTRY=$(oc registry info --public) &&
+	    test -n "$REGISTRY" &&
+	    oc registry login --insecure --registry $REGISTRY 1>&2 &&
+	    echo $REGISTRY &&
+	    exit 0
+	echo "retry registry login.." 1>&2
+	sleep 6
+    done
 fi
 exit 1

d34fbfe2312c0b9c6c36c3f8ce6eed781a441237
Jeff Cantrill <jcantril@redhat.com>
2021-10-06 12:35:28 -0400

    modify forward to kafka to table

--- test/e2e/logforwarding/kafka/forward_to_kafka_test.go
+++ test/e2e/logforwarding/kafka/forward_to_kafka_test.go
@@ -3,8 +3,10 @@ package kafka
 import (
 	"fmt"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"runtime"
+	"time"
 
 	"github.com/ViaQ/logerr/log"
 	loggingv1 "github.com/openshift/cluster-logging-operator/pkg/apis/logging/v1"
@@ -27,110 +29,68 @@ var _ = Describe("[ClusterLogForwarder] Forwards logs", func() {
 			log.Error(err, "unable to deploy log generator.")
 		}
 	})
-	Describe("when the output is a third-party managed kafka", func() {
 
-		Context("split app, audit and infra on different topics", func() {
-			BeforeEach(func() {
-				topics := []string{kafka.AppLogsTopic, kafka.AuditLogsTopic, kafka.InfraLogsTopic}
-				app, err = e2e.DeployKafkaReceiver(topics)
-				if err != nil {
-					Fail(fmt.Sprintf("Unable to deploy kafka receiver: %v", err))
-				}
+	DescribeTable("when sending logs to a third-party managed kafka", func(topic, inputName string, hasLogs func(time.Duration) (bool, error)) {
+		topics := []string{topic}
+		app, err = e2e.DeployKafkaReceiver(topics)
+		if err != nil {
+			Fail(fmt.Sprintf("Unable to deploy kafka receiver: %v", err))
+		}
 
-				cr := helpers.NewClusterLogging(helpers.ComponentTypeCollector)
-				if err := e2e.CreateClusterLogging(cr); err != nil {
-					Fail(fmt.Sprintf("Unable to create an instance of cluster logging: %v", err))
-				}
-				forwarder := &loggingv1.ClusterLogForwarder{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       loggingv1.ClusterLogForwarderKind,
-						APIVersion: loggingv1.SchemeGroupVersion.String(),
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "instance",
-					},
-					Spec: loggingv1.ClusterLogForwarderSpec{
-						Outputs: []loggingv1.OutputSpec{
-							{
-								Name: fmt.Sprintf("%s-app-out", kafka.DeploymentName),
-								Type: loggingv1.OutputTypeKafka,
-								URL:  fmt.Sprintf("tls://%s", e2e.LogStores[app.Name].ClusterLocalEndpoint()),
-								OutputTypeSpec: loggingv1.OutputTypeSpec{
-									Kafka: &loggingv1.Kafka{
-										Topic: kafka.AppLogsTopic,
-									},
-								},
-								Secret: &loggingv1.OutputSecretSpec{
-									Name: kafka.DeploymentName,
-								},
-							},
-							{
-								Name: fmt.Sprintf("%s-audit-out", kafka.DeploymentName),
-								Type: loggingv1.OutputTypeKafka,
-								URL:  fmt.Sprintf("tls://%s", e2e.LogStores[app.Name].ClusterLocalEndpoint()),
-								OutputTypeSpec: loggingv1.OutputTypeSpec{
-									Kafka: &loggingv1.Kafka{
-										Topic: kafka.AuditLogsTopic,
-									},
-								},
-								Secret: &loggingv1.OutputSecretSpec{
-									Name: kafka.DeploymentName,
-								},
-							},
-							{
-								Name: fmt.Sprintf("%s-infra-out", kafka.DeploymentName),
-								Type: loggingv1.OutputTypeKafka,
-								URL:  fmt.Sprintf("tls://%s", e2e.LogStores[app.Name].ClusterLocalEndpoint()),
-								OutputTypeSpec: loggingv1.OutputTypeSpec{
-									Kafka: &loggingv1.Kafka{
-										Topic: kafka.InfraLogsTopic,
-									},
-								},
-								Secret: &loggingv1.OutputSecretSpec{
-									Name: kafka.DeploymentName,
-								},
+		cr := helpers.NewClusterLogging(helpers.ComponentTypeCollector)
+		if err := e2e.CreateClusterLogging(cr); err != nil {
+			Fail(fmt.Sprintf("Unable to create an instance of cluster logging: %v", err))
+		}
+		forwarder := &loggingv1.ClusterLogForwarder{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       loggingv1.ClusterLogForwarderKind,
+				APIVersion: loggingv1.SchemeGroupVersion.String(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "instance",
+			},
+			Spec: loggingv1.ClusterLogForwarderSpec{
+				Outputs: []loggingv1.OutputSpec{
+					{
+						Name: kafka.DeploymentName,
+						Type: loggingv1.OutputTypeKafka,
+						URL:  fmt.Sprintf("tls://%s", e2e.LogStores[app.Name].ClusterLocalEndpoint()),
+						OutputTypeSpec: loggingv1.OutputTypeSpec{
+							Kafka: &loggingv1.Kafka{
+								Topic: topic,
 							},
 						},
-						Pipelines: []loggingv1.PipelineSpec{
-							{
-								Name:       "kafka-app",
-								InputRefs:  []string{loggingv1.InputNameApplication},
-								OutputRefs: []string{fmt.Sprintf("%s-app-out", kafka.DeploymentName)},
-							},
-							{
-								Name:       "kafka-audit",
-								InputRefs:  []string{loggingv1.InputNameAudit},
-								OutputRefs: []string{fmt.Sprintf("%s-audit-out", kafka.DeploymentName)},
-							},
-							{
-								Name:       "kafka-infra",
-								InputRefs:  []string{loggingv1.InputNameInfrastructure},
-								OutputRefs: []string{fmt.Sprintf("%s-infra-out", kafka.DeploymentName)},
-							},
+						Secret: &loggingv1.OutputSecretSpec{
+							Name: kafka.DeploymentName,
 						},
 					},
-				}
-				if err := e2e.CreateClusterLogForwarder(forwarder); err != nil {
-					Fail(fmt.Sprintf("Unable to create an instance of ClusterLogForwarder: %v", err))
-				}
-				components := []helpers.LogComponentType{helpers.ComponentTypeCollector}
-				for _, component := range components {
-					if err := e2e.WaitFor(component); err != nil {
-						Fail(fmt.Sprintf("Failed waiting for component %s to be ready: %v", component, err))
-					}
-				}
+				},
+				Pipelines: []loggingv1.PipelineSpec{
+					{
+						Name:       topic,
+						InputRefs:  []string{inputName},
+						OutputRefs: []string{kafka.DeploymentName},
+					},
+				},
+			},
+		}
+		if err := e2e.CreateClusterLogForwarder(forwarder); err != nil {
+			Fail(fmt.Sprintf("Unable to create an instance of ClusterLogForwarder: %v", err))
+		}
+		components := []helpers.LogComponentType{helpers.ComponentTypeCollector}
+		for _, component := range components {
+			if err := e2e.WaitFor(component); err != nil {
+				Fail(fmt.Sprintf("Failed waiting for component %s to be ready: %v", component, err))
+			}
+		}
+		Expect(hasLogs(helpers.DefaultWaitForLogsTimeout)).To(BeTrue())
+	},
+		Entry("should store application logs", kafka.AppLogsTopic, loggingv1.InputNameApplication, e2e.LogStores[app.Name].HasApplicationLogs),
+		Entry("should store infrastructure logs", kafka.InfraLogsTopic, loggingv1.InputNameInfrastructure, e2e.LogStores[app.Name].HasInfraStructureLogs),
+		Entry("should store audit logs", kafka.AuditLogsTopic, loggingv1.InputNameAudit, e2e.LogStores[app.Name].HasAuditLogs),
+	)
 
-			})
-
-			It("should send logs to the forward.Output logstore", func() {
-				Expect(e2e.LogStores[app.Name].HasInfraStructureLogs(helpers.DefaultWaitForLogsTimeout)).To(BeTrue(), "Expected to find stored infrastructure logs")
-				Expect(e2e.LogStores[app.Name].HasApplicationLogs(helpers.DefaultWaitForLogsTimeout)).To(BeTrue(), "Expected to find stored application logs")
-				Expect(e2e.LogStores[app.Name].HasAuditLogs(helpers.DefaultWaitForLogsTimeout)).To(BeTrue(), "Expected to find stored audit logs")
-			})
-
-			AfterEach(func() {
-				e2e.Cleanup()
-			})
-		})
+	AfterEach(func() {
+		e2e.Cleanup()
 	})
 })

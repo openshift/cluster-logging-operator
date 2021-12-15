550f83ec88891e02e72480be7ded527c620aa087
Jeff Cantrill <jcantril@redhat.com>
2021-12-15 16:53:12 -0500

    LOG-2066: Upgrade the metric dashboard when it changes

--- internal/runtime/configmap.go
+++ internal/runtime/configmap.go
@@ -17,3 +17,17 @@ func (builder *ConfigMapBuilder) Add(key, value string) *ConfigMapBuilder {
 	builder.ConfigMap.Data[key] = value
 	return builder
 }
+func (builder *ConfigMapBuilder) AddLabel(key, value string) *ConfigMapBuilder {
+	if builder.ConfigMap.Labels == nil {
+		builder.ConfigMap.Labels = map[string]string{}
+	}
+	builder.ConfigMap.Labels[key] = value
+	return builder
+}
+func (builder *ConfigMapBuilder) AddAnnotation(key, value string) *ConfigMapBuilder {
+	if builder.ConfigMap.Annotations == nil {
+		builder.ConfigMap.Annotations = map[string]string{}
+	}
+	builder.ConfigMap.Annotations[key] = value
+	return builder
+}
---
+++ internal/utils/comparators/configmaps/suite_test.go
@@ -0,0 +1,13 @@
+package configmaps_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "[internal][utils][comparators][configmaps] Suite")
+}

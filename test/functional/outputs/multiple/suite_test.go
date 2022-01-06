3d31925dbe5181f6fc7b1fffd5f51e646895ba5f
Alan Conway <aconway@redhat.com>
2022-01-06 13:30:04 -0500

    LOG-2117 Remove deprecated Ginkgo custom reporters

--- test/functional/outputs/multiple/suite_test.go
+++ test/functional/outputs/multiple/suite_test.go
@@ -4,14 +4,10 @@ import (
 	"testing"
 
 	. "github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 )
 
 func TestFunctionalOutputs(t *testing.T) {
 	RegisterFailHandler(Fail)
-
-	tc := "[Functional][Outputs][Multiple] Test Suite"
-	jr := reporters.NewJUnitReporter("/tmp/artifacts/junit/junit-functional-outputs-multiple.xml")
-	RunSpecsWithDefaultAndCustomReporters(t, tc, []Reporter{jr})
+	RunSpecs(t, "[Functional][Outputs][Multiple] Test Suite")
 }

8b8400be478788415a74665dab6b20fcb7fdce06
Vimal Kumar <vimal78@gmail.com>
2022-02-04 02:14:11 +0530

    LOG-2193: Add pipeline labels to Vector produyced record

--- internal/generator/vector/inputs_to_pipeline.go
+++ internal/generator/vector/inputs_to_pipeline.go
@@ -1,6 +1,9 @@
 package vector
 
 import (
+	"encoding/json"
+	"fmt"
+
 	logging "github.com/openshift/cluster-logging-operator/apis/logging/v1"
 	"github.com/openshift/cluster-logging-operator/internal/generator"
 	. "github.com/openshift/cluster-logging-operator/internal/generator/vector/elements"
@@ -10,10 +13,15 @@ import (
 func InputsToPipelines(spec *logging.ClusterLogForwarderSpec, op generator.Options) []generator.Element {
 	el := []generator.Element{}
 	for _, p := range spec.Pipelines {
+		vrl := SrcPassThrough
+		if p.Labels != nil && len(p.Labels) != 0 {
+			s, _ := json.Marshal(p.Labels)
+			vrl = fmt.Sprintf(".openshift.labels = %s", s)
+		}
 		r := Remap{
 			ComponentID: p.Name,
 			Inputs:      helpers.MakeInputs(p.InputRefs...),
-			VRL:         SrcPassThrough,
+			VRL:         vrl,
 		}
 		el = append(el, r)
 

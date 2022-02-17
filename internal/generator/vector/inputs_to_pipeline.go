7e6e5ebc22f8d59b9b88cb4140e0bf1f37677e59
Vimal Kumar <vimal78@gmail.com>
2022-02-17 21:29:28 +0530

    LOG-2255: Vector: Support Namespace filtering

--- internal/generator/vector/inputs_to_pipeline.go
+++ internal/generator/vector/inputs_to_pipeline.go
@@ -10,17 +10,30 @@ import (
 	"github.com/openshift/cluster-logging-operator/internal/generator/vector/helpers"
 )
 
+var (
+	UserDefinedInput = fmt.Sprintf("%s.%%s", RouteApplicationLogs)
+)
+
 func InputsToPipelines(spec *logging.ClusterLogForwarderSpec, op generator.Options) []generator.Element {
 	el := []generator.Element{}
+	userDefined := spec.InputMap()
 	for _, p := range spec.Pipelines {
 		vrl := SrcPassThrough
 		if p.Labels != nil && len(p.Labels) != 0 {
 			s, _ := json.Marshal(p.Labels)
 			vrl = fmt.Sprintf(".openshift.labels = %s", s)
 		}
+		inputs := []string{}
+		for _, i := range p.InputRefs {
+			if _, ok := userDefined[i]; ok {
+				inputs = append(inputs, fmt.Sprintf(UserDefinedInput, i))
+			} else {
+				inputs = append(inputs, i)
+			}
+		}
 		r := Remap{
 			ComponentID: p.Name,
-			Inputs:      helpers.MakeInputs(p.InputRefs...),
+			Inputs:      helpers.MakeInputs(inputs...),
 			VRL:         vrl,
 		}
 		el = append(el, r)

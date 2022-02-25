efd20f7d986482438741ec61f0355edf88382525
Jeff Cantrill <jcantril@redhat.com>
2022-02-25 15:00:52 -0500

    LOG-2434: Update fluentd references to 1.14.5

--- olm_deploy/scripts/registry-init.sh
+++ olm_deploy/scripts/registry-init.sh
@@ -9,7 +9,7 @@ echo -e "\n\n"
 
 # update the manifest with the image built by ci
 sed -i "s,quay.io/openshift-logging/cluster-logging-operator:latest,${IMAGE_CLUSTER_LOGGING_OPERATOR}," /manifests/*/*clusterserviceversion.yaml
-sed -i "s,quay.io/openshift-logging/fluentd:1.7.4,${IMAGE_LOGGING_FLUENTD}," /manifests/*/*clusterserviceversion.yaml
+sed -i "s,quay.io/openshift-logging/fluentd:1.14.5,${IMAGE_LOGGING_FLUENTD}," /manifests/*/*clusterserviceversion.yaml
 sed -i "s,quay.io/openshift-logging/vector:0.14.1,${IMAGE_LOGGING_VECTOR}," /manifests/*/*clusterserviceversion.yaml
 sed -i "s,quay.io/openshift-logging/log-file-metric-exporter:1.0,${IMAGE_LOG_FILE_METRIC_EXPORTER}," /manifests/*/*clusterserviceversion.yaml
 

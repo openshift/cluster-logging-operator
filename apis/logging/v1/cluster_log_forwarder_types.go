91cb2c2b57c1b9905d0a80087ba895ef070b4d93
Alan Conway <aconway@redhat.com>
2022-03-30 14:51:29 -0400

    NO-JIRA: Add tls.insecure for vector TLS.
    - Add tls.insecure key (with any value) to kafka generator.
      - accept self-signed certs.
      - accept certs that don't match the target hostname.
    - Fixed some minor issues in Kafka config generator
    
    NOTE: need a JIRA to implement tls.insecure uniformly for all outputs.

--- apis/logging/v1/cluster_log_forwarder_types.go
+++ apis/logging/v1/cluster_log_forwarder_types.go
@@ -162,7 +162,7 @@ type OutputSpec struct {
 	//
 	// Transport Layer Security (TLS)
 	//
-	// Using a TLS URL ('https://...' or 'ssl://...') without any secret enables basic TLS:
+	// Using a TLS URL ('https://...' or 'tls://...') without any secret enables basic TLS:
 	// client authenticates server using system default certificate authority.
 	//
 	// Additional TLS features are enabled by referencing a Secret with the following optional fields in its spec.data.
@@ -172,6 +172,10 @@ type OutputSpec struct {
 	//   `tls.key`: Private key to unlock the client certificate. Requires `tls.crt`
 	//   `passphrase`: Passphrase to decode an encoded TLS private key. Requires tls.key.
 	//   `ca-bundle.crt`: Custom CA to validate certificates.
+	//
+	//   `tls.insecure`: If this key is present (with any value) accept server certificates that are self-signed or have an incorrect hostname.
+	//     This is INSECURE, the connection is susceptible to man-in-the-middle attacks.
+	//     This is ONLY for development and testing when valid certificates are not available.
 	//
 	// Username and Password
 	//

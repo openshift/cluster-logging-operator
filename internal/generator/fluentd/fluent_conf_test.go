bfb029c7e7bba74a97de2c2db42a38ec136f8a2d
Jeff Cantrill <jcantril@redhat.com>
2022-08-26 07:21:05 -0400

    LOG-2792: fix fluent periodically not following files by adding follow_inode

--- internal/generator/fluentd/fluent_conf_test.go
+++ internal/generator/fluentd/fluent_conf_test.go
@@ -242,6 +242,7 @@ var _ = Describe("Generating fluentd config", func() {
   path "/var/log/pods/*/*/*.log"
   exclude_path ["/var/log/pods/openshift-logging_collector-*/*/*.log", "/var/log/pods/openshift-logging_elasticsearch-*/*/*.log", "/var/log/pods/openshift-logging_kibana-*/*/*.log", "/var/log/pods/*/*/*.gz", "/var/log/pods/*/*/*.tmp"]
   pos_file "/var/lib/fluentd/pos/es-containers.log.pos"
+  follow_inodes true
   refresh_interval 5
   rotate_wait 5
   tag kubernetes.*
@@ -962,6 +963,7 @@ var _ = Describe("Generating fluentd config", func() {
   path "/var/log/pods/*/*/*.log"
   exclude_path ["/var/log/pods/openshift-logging_collector-*/*/*.log", "/var/log/pods/openshift-logging_elasticsearch-*/*/*.log", "/var/log/pods/openshift-logging_kibana-*/*/*.log", "/var/log/pods/*/*/*.gz", "/var/log/pods/*/*/*.tmp"]
   pos_file "/var/lib/fluentd/pos/es-containers.log.pos"
+  follow_inodes true
   refresh_interval 5
   rotate_wait 5
   tag kubernetes.*
@@ -1667,6 +1669,7 @@ var _ = Describe("Generating fluentd config", func() {
   path "/var/log/pods/*/*/*.log"
   exclude_path ["/var/log/pods/openshift-logging_collector-*/*/*.log", "/var/log/pods/openshift-logging_elasticsearch-*/*/*.log", "/var/log/pods/openshift-logging_kibana-*/*/*.log", "/var/log/pods/*/*/*.gz", "/var/log/pods/*/*/*.tmp"]
   pos_file "/var/lib/fluentd/pos/es-containers.log.pos"
+  follow_inodes true
   refresh_interval 5
   rotate_wait 5
   tag kubernetes.*
@@ -2317,6 +2320,7 @@ var _ = Describe("Generating fluentd config", func() {
   path "/var/log/pods/*/*/*.log"
   exclude_path ["/var/log/pods/openshift-logging_collector-*/*/*.log", "/var/log/pods/openshift-logging_elasticsearch-*/*/*.log", "/var/log/pods/openshift-logging_kibana-*/*/*.log", "/var/log/pods/*/*/*.gz", "/var/log/pods/*/*/*.tmp"]
   pos_file "/var/lib/fluentd/pos/es-containers.log.pos"
+  follow_inodes true
   refresh_interval 5
   rotate_wait 5
   tag kubernetes.*
@@ -2683,6 +2687,7 @@ var _ = Describe("Generating fluentd config", func() {
   path "/var/log/pods/*/*/*.log"
   exclude_path ["/var/log/pods/openshift-logging_collector-*/*/*.log", "/var/log/pods/openshift-logging_elasticsearch-*/*/*.log", "/var/log/pods/openshift-logging_kibana-*/*/*.log", "/var/log/pods/*/*/*.gz", "/var/log/pods/*/*/*.tmp"]
   pos_file "/var/lib/fluentd/pos/es-containers.log.pos"
+  follow_inodes true
   refresh_interval 5
   rotate_wait 5
   tag kubernetes.*
@@ -2704,6 +2709,7 @@ var _ = Describe("Generating fluentd config", func() {
   @label @INGRESS
   path "/var/log/audit/audit.log"
   pos_file "/var/lib/fluentd/pos/audit.log.pos"
+  follow_inodes true
   tag linux-audit.log
   <parse>
     @type viaq_host_audit
@@ -2717,6 +2723,7 @@ var _ = Describe("Generating fluentd config", func() {
   @label @INGRESS
   path "/var/log/kube-apiserver/audit.log"
   pos_file "/var/lib/fluentd/pos/kube-apiserver.audit.log.pos"
+  follow_inodes true
   tag k8s-audit.log
   <parse>
     @type json
@@ -2734,6 +2741,7 @@ var _ = Describe("Generating fluentd config", func() {
   @label @INGRESS
   path /var/log/oauth-apiserver/audit.log,/var/log/openshift-apiserver/audit.log
   pos_file /var/lib/fluentd/pos/oauth-apiserver.audit.log
+  follow_inodes true
   tag openshift-audit.log
   <parse>
     @type json
@@ -2751,6 +2759,7 @@ var _ = Describe("Generating fluentd config", func() {
   @label @INGRESS
   path "/var/log/ovn/acl-audit-log.log"
   pos_file "/var/lib/fluentd/pos/acl-audit-log.pos"
+  follow_inodes true
   tag ovn-audit.log
   refresh_interval 5
   rotate_wait 5
@@ -3753,6 +3762,7 @@ inputs:
   path "/var/log/pods/*/*/*.log"
   exclude_path ["/var/log/pods/openshift-logging_collector-*/*/*.log", "/var/log/pods/openshift-logging_elasticsearch-*/*/*.log", "/var/log/pods/openshift-logging_kibana-*/*/*.log", "/var/log/pods/*/*/*.gz", "/var/log/pods/*/*/*.tmp"]
   pos_file "/var/lib/fluentd/pos/es-containers.log.pos"
+  follow_inodes true
   refresh_interval 5
   rotate_wait 5
   tag kubernetes.*

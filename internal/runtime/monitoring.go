50b79e4191210a7d2800b0d61a69e365d0e3d051
Jeff Cantrill <jcantril@redhat.com>
2022-09-26 11:57:39 -0400

    LOG-3109: fix CLO setting wrong ServiceMonitor NS

--- internal/runtime/monitoring.go
+++ internal/runtime/monitoring.go
@@ -5,14 +5,14 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func NewServiceMonitor(serviceMonitorName, namespace string) *monitoringv1.ServiceMonitor {
+func NewServiceMonitor(namespace, name string) *monitoringv1.ServiceMonitor {
 	return &monitoringv1.ServiceMonitor{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       monitoringv1.ServiceMonitorsKind,
 			APIVersion: monitoringv1.SchemeGroupVersion.String(),
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      serviceMonitorName,
+			Name:      name,
 			Namespace: namespace,
 		},
 		Spec: monitoringv1.ServiceMonitorSpec{},

41dd66d468d821e97e286527f9993f0176d82052
Jeff Cantrill <jcantril@redhat.com>
2022-10-10 16:31:26 -0400

    LOG-3159: must-gather based upon whats deployed

--- test/e2e/logforwarding/cleanup.sh
+++ test/e2e/logforwarding/cleanup.sh
@@ -2,18 +2,25 @@
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../../../hack/testing-olm/utils"
 artifact_dir=$1
 GENERATOR_NS=$2
+LOGGING_NS=${3:-openshift-logging}
 runtime=$(date +%s)
 mkdir -p "$artifact_dir/$runtime" ||:
-gather_logging_resources "openshift-logging" "$artifact_dir" "$runtime"
+gather_logging_resources "$LOGGING_NS" "$artifact_dir" "$runtime"
 
-oc -n "$GENERATOR_NS" describe deployment/log-generator  > "$artifact_dir/$runtime/log-generator.describe" ||:
-oc -n "$GENERATOR_NS" logs deployment/log-generator  > "$artifact_dir/$runtime/log-generator.logs" ||:
-oc -n "$GENERATOR_NS" get deployment/log-generator -o yaml > "$artifact_dir/$runtime/log-generator.deployment.yaml" ||:
-oc -n "$GENERATOR_NS" get pods -o yaml > "$artifact_dir/$runtime/log-generator.pods.yaml" ||:
+if [ "$(oc -n "$GENERATOR_NS" describe deployment/log-generator --ignore-not-found --no-headers)" != "" ] ; then
+  oc -n "$GENERATOR_NS" describe deployment/log-generator  > "$artifact_dir/$runtime/log-generator.describe" ||:
+  oc -n "$GENERATOR_NS" logs deployment/log-generator  > "$artifact_dir/$runtime/log-generator.logs" ||:
+  oc -n "$GENERATOR_NS" get deployment/log-generator -o yaml > "$artifact_dir/$runtime/log-generator.deployment.yaml" ||:
+  oc -n "$GENERATOR_NS" get pods -o yaml > "$artifact_dir/$runtime/log-generator.pods.yaml" ||:
+fi
 
-oc -n openshift-logging exec $(oc -n openshift-logging get pods -l component=syslog-receiver -o name| sed 's/pod\///') -- tail -n 20000 /var/log/infra.log > "$artifact_dir/$runtime/syslog-receiver.log" ||:
-oc -n openshift-logging exec $(oc -n openshift-logging get pods -l component=syslog-receiver -o name| sed 's/pod\///') -- cat /rsyslog/etc/rsyslog.conf > "$artifact_dir/$runtime/syslog-receiver.conf" ||:
-for pod in $(oc -n openshift-logging get pods -llogging-infra=kafka -oname| sed 's/pod\///')
+if [ "$(oc -n $LOGGING_NS get pods -l component=syslog-receiver -o name --ignore-not-found --no-headers)" != "" ] ; then
+  pod_name=$(oc -n $LOGGING_NS get pods -l component=syslog-receiver -o name| sed 's/pod\///')
+  oc -n $LOGGING_NS exec $pod_name -- tail -n 20000 /var/log/infra.log > "$artifact_dir/$runtime/syslog-receiver.log" ||:
+  oc -n $LOGGING_NS exec $pod_name -- cat /rsyslog/etc/rsyslog.conf > "$artifact_dir/$runtime/syslog-receiver.conf" ||:
+fi
+
+for pod in $(oc -n $LOGGING_NS pods -llogging-infra=kafka -oname| sed 's/pod\///')
 do
-    oc -n openshift-logging exec $pod -- tail -n 5000 /shared/consumed.logs > "$artifact_dir/$runtime/$pod.logs" ||:
+    oc -n $LOGGING_NS exec $pod -- tail -n 5000 /shared/consumed.logs > "$artifact_dir/$runtime/$pod.logs" ||:
 done

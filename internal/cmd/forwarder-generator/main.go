7e3e38a5e5e63f3b4cb854d98e06dc9f9c5dbcaa
Jeff Cantrill <jcantril@redhat.com>
2023-03-17 11:12:15 -0400

    add secrets to forwarder generator

--- internal/cmd/forwarder-generator/main.go
+++ internal/cmd/forwarder-generator/main.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"github.com/openshift/cluster-logging-operator/internal/runtime"
 	"io/ioutil"
+	corev1 "k8s.io/api/core/v1"
 	"os"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"strings"
 
 	log "github.com/ViaQ/logerr/v2/log/static"
 	logging "github.com/openshift/cluster-logging-operator/apis/logging/v1"
@@ -23,6 +26,7 @@ func main() {
 	includeDefaultLogStore := flag.Bool("include-default-store", true, "Include the default storage when generating the config")
 	debugOutput := flag.Bool("debug-output", false, "Generate config normally, but replace output plugins with @stdout plugin, so that records can be printed in collector logs.")
 	colltype := flag.String("collector", "fluentd", "collector type: fluentd or vector")
+	secretsFlag := flag.String("secrets", "", "colon delimited list of secrets in the form of name=key1,key1")
 	help := flag.Bool("help", false, "This message")
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
@@ -55,14 +59,42 @@ func main() {
 
 	content, err := reader()
 	if err != nil {
-		log.Error(err, "Error Unmarshalling file ", "file", yamlFile)
+		log.Error(err, "Error reading file ", "file", yamlFile)
 		os.Exit(1)
 	}
 
-	log.Info("Finished reading yaml", "content", string(content))
+	clfYaml := string(content)
+	log.Info("Finished reading yaml", "content", clfYaml)
+	clf, err := forwarder.UnMarshalClusterLogForwarder(clfYaml)
+	if err != nil {
+		log.Error(err, "Error UnMarshalling CLF", "file", yamlFile)
+		os.Exit(1)
+	}
+	log.V(2).Info("Unmarshalled", "forwarder", clfYaml)
 
-	client := fake.NewClientBuilder().Build()
-	generatedConfig, err := forwarder.Generate(logCollectorType, string(content), *includeDefaultLogStore, *debugOutput, client)
+	var secrets []*corev1.Secret
+	if *secretsFlag != "" {
+		for _, raw := range strings.Split(*secretsFlag, ":") {
+			rawentry := strings.Split(raw, "=")
+			if len(rawentry) == 2 {
+				name := rawentry[0]
+				secret := runtime.NewSecret(clf.Namespace, name, nil)
+				for _, key := range strings.Split(rawentry[1], ",") {
+					secret.Data[key] = []byte(key)
+				}
+				secrets = append(secrets, secret)
+			} else {
+				log.V(1).Info("Unable to create rawentry from arg", "entry", raw)
+			}
+		}
+
+	}
+	clientBuilder := fake.NewClientBuilder()
+	for _, entry := range secrets {
+		clientBuilder.WithRuntimeObjects(entry)
+	}
+	client := clientBuilder.Build()
+	generatedConfig, err := forwarder.Generate(logCollectorType, clfYaml, *includeDefaultLogStore, *debugOutput, client)
 	if err != nil {
 		log.Error(err, "Unable to generate log configuration")
 		os.Exit(1)

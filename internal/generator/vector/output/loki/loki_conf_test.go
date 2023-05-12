3a68f7dfcf47c555163323646176e59c48e1df8c
Vitalii Parfonov <vparfono@redhat.com>
2023-05-12 13:18:51 +0300

    LOG-3445: add unit test for verification correct config in case tls.insecureSkipVerify=true
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- internal/generator/vector/output/loki/loki_conf_test.go
+++ internal/generator/vector/output/loki/loki_conf_test.go
@@ -70,84 +70,84 @@ var _ = Describe("Generate vector config", func() {
 				},
 			},
 			ExpectedConf: `
-[transforms.loki_receiver_remap]
-type = "remap"
-inputs = ["application"]
-source = '''
-  del(.tag)
-'''
-
-[transforms.loki_receiver_dedot]
-type = "lua"
-inputs = ["loki_receiver_remap"]
-version = "2"
-hooks.init = "init"
-hooks.process = "process"
-source = '''
-    function init()
-        count = 0
-    end
-    function process(event, emit)
-        count = count + 1
-        event.log.openshift.sequence = count
-        if event.log.kubernetes == nil then
-            emit(event)
-            return
-        end
-        if event.log.kubernetes.labels == nil then
-            emit(event)
-            return
-        end
-		dedot(event.log.kubernetes.namespace_labels)
-        dedot(event.log.kubernetes.labels)
-        emit(event)
-    end
-
-    function dedot(map)
-        if map == nil then
-            return
-        end
-        local new_map = {}
-        local changed_keys = {}
-        for k, v in pairs(map) do
-            local dedotted = string.gsub(k, "[./]", "_")
-            if dedotted ~= k then
-                new_map[dedotted] = v
-                changed_keys[k] = true
-            end
-        end
-        for k in pairs(changed_keys) do
-            map[k] = nil
-        end
-        for k, v in pairs(new_map) do
-            map[k] = v
-        end
-    end
-'''
-
-[sinks.loki_receiver]
-type = "loki"
-inputs = ["loki_receiver_dedot"]
-endpoint = "https://logs-us-west1.grafana.net"
-out_of_order_action = "accept"
-healthcheck.enabled = false
-
-[sinks.loki_receiver.encoding]
-codec = "json"
-
-[sinks.loki_receiver.labels]
-kubernetes_container_name = "{{kubernetes.container_name}}"
-kubernetes_host = "${VECTOR_SELF_NODE_NAME}"
-kubernetes_namespace_name = "{{kubernetes.namespace_name}}"
-kubernetes_pod_name = "{{kubernetes.pod_name}}"
-log_type = "{{log_type}}"
-
-# Basic Auth Config
-[sinks.loki_receiver.auth]
-strategy = "basic"
-user = "username"
-password = "password"
-`,
+		[transforms.loki_receiver_remap]
+		type = "remap"
+		inputs = ["application"]
+		source = '''
+		 del(.tag)
+		'''
+		
+		[transforms.loki_receiver_dedot]
+		type = "lua"
+		inputs = ["loki_receiver_remap"]
+		version = "2"
+		hooks.init = "init"
+		hooks.process = "process"
+		source = '''
+		   function init()
+		       count = 0
+		   end
+		   function process(event, emit)
+		       count = count + 1
+		       event.log.openshift.sequence = count
+		       if event.log.kubernetes == nil then
+		           emit(event)
+		           return
+		       end
+		       if event.log.kubernetes.labels == nil then
+		           emit(event)
+		           return
+		       end
+				dedot(event.log.kubernetes.namespace_labels)
+		       dedot(event.log.kubernetes.labels)
+		       emit(event)
+		   end
+		
+		   function dedot(map)
+		       if map == nil then
+		           return
+		       end
+		       local new_map = {}
+		       local changed_keys = {}
+		       for k, v in pairs(map) do
+		           local dedotted = string.gsub(k, "[./]", "_")
+		           if dedotted ~= k then
+		               new_map[dedotted] = v
+		               changed_keys[k] = true
+		           end
+		       end
+		       for k in pairs(changed_keys) do
+		           map[k] = nil
+		       end
+		       for k, v in pairs(new_map) do
+		           map[k] = v
+		       end
+		   end
+		'''
+		
+		[sinks.loki_receiver]
+		type = "loki"
+		inputs = ["loki_receiver_dedot"]
+		endpoint = "https://logs-us-west1.grafana.net"
+		out_of_order_action = "accept"
+		healthcheck.enabled = false
+		
+		[sinks.loki_receiver.encoding]
+		codec = "json"
+		
+		[sinks.loki_receiver.labels]
+		kubernetes_container_name = "{{kubernetes.container_name}}"
+		kubernetes_host = "${VECTOR_SELF_NODE_NAME}"
+		kubernetes_namespace_name = "{{kubernetes.namespace_name}}"
+		kubernetes_pod_name = "{{kubernetes.pod_name}}"
+		log_type = "{{log_type}}"
+		
+		# Basic Auth Config
+		[sinks.loki_receiver.auth]
+		strategy = "basic"
+		user = "username"
+		password = "password"
+		`,
 		}),
 		Entry("with custom labels", helpers.ConfGenerateTest{
 			CLFSpec: logging.ClusterLogForwarderSpec{
@@ -174,82 +174,82 @@ password = "password"
 				},
 			},
 			ExpectedConf: `
-[transforms.loki_receiver_remap]
-type = "remap"
-inputs = ["application"]
-source = '''
-  del(.tag)
-'''
-
-[transforms.loki_receiver_dedot]
-type = "lua"
-inputs = ["loki_receiver_remap"]
-version = "2"
-hooks.init = "init"
-hooks.process = "process"
-source = '''
-    function init()
-        count = 0
-    end
-    function process(event, emit)
-        count = count + 1
-        event.log.openshift.sequence = count
-        if event.log.kubernetes == nil then
-            emit(event)
-            return
-        end
-        if event.log.kubernetes.labels == nil then
-            emit(event)
-            return
-        end
-		dedot(event.log.kubernetes.namespace_labels)
-        dedot(event.log.kubernetes.labels)
-        emit(event)
-    end
-
-    function dedot(map)
-        if map == nil then
-            return
-        end
-        local new_map = {}
-        local changed_keys = {}
-        for k, v in pairs(map) do
-            local dedotted = string.gsub(k, "[./]", "_")
-            if dedotted ~= k then
-                new_map[dedotted] = v
-                changed_keys[k] = true
-            end
-        end
-        for k in pairs(changed_keys) do
-            map[k] = nil
-        end
-        for k, v in pairs(new_map) do
-            map[k] = v
-        end
-    end
-'''
-
-[sinks.loki_receiver]
-type = "loki"
-inputs = ["loki_receiver_dedot"]
-endpoint = "https://logs-us-west1.grafana.net"
-out_of_order_action = "accept"
-healthcheck.enabled = false
-
-[sinks.loki_receiver.encoding]
-codec = "json"
-
-[sinks.loki_receiver.labels]
-kubernetes_container_name = "{{kubernetes.container_name}}"
-kubernetes_host = "${VECTOR_SELF_NODE_NAME}"
-kubernetes_labels_app = "{{kubernetes.labels.app}}"
-
-# Basic Auth Config
-[sinks.loki_receiver.auth]
-strategy = "basic"
-user = "username"
-password = "password"
-`,
+		[transforms.loki_receiver_remap]
+		type = "remap"
+		inputs = ["application"]
+		source = '''
+		 del(.tag)
+		'''
+		
+		[transforms.loki_receiver_dedot]
+		type = "lua"
+		inputs = ["loki_receiver_remap"]
+		version = "2"
+		hooks.init = "init"
+		hooks.process = "process"
+		source = '''
+		   function init()
+		       count = 0
+		   end
+		   function process(event, emit)
+		       count = count + 1
+		       event.log.openshift.sequence = count
+		       if event.log.kubernetes == nil then
+		           emit(event)
+		           return
+		       end
+		       if event.log.kubernetes.labels == nil then
+		           emit(event)
+		           return
+		       end
+				dedot(event.log.kubernetes.namespace_labels)
+		       dedot(event.log.kubernetes.labels)
+		       emit(event)
+		   end
+		
+		   function dedot(map)
+		       if map == nil then
+		           return
+		       end
+		       local new_map = {}
+		       local changed_keys = {}
+		       for k, v in pairs(map) do
+		           local dedotted = string.gsub(k, "[./]", "_")
+		           if dedotted ~= k then
+		               new_map[dedotted] = v
+		               changed_keys[k] = true
+		           end
+		       end
+		       for k in pairs(changed_keys) do
+		           map[k] = nil
+		       end
+		       for k, v in pairs(new_map) do
+		           map[k] = v
+		       end
+		   end
+		'''
+		
+		[sinks.loki_receiver]
+		type = "loki"
+		inputs = ["loki_receiver_dedot"]
+		endpoint = "https://logs-us-west1.grafana.net"
+		out_of_order_action = "accept"
+		healthcheck.enabled = false
+		
+		[sinks.loki_receiver.encoding]
+		codec = "json"
+		
+		[sinks.loki_receiver.labels]
+		kubernetes_container_name = "{{kubernetes.container_name}}"
+		kubernetes_host = "${VECTOR_SELF_NODE_NAME}"
+		kubernetes_labels_app = "{{kubernetes.labels.app}}"
+		
+		# Basic Auth Config
+		[sinks.loki_receiver.auth]
+		strategy = "basic"
+		user = "username"
+		password = "password"
+		`,
 		}),
 		Entry("with tenant id", helpers.ConfGenerateTest{
 			CLFSpec: logging.ClusterLogForwarderSpec{
@@ -276,86 +276,86 @@ password = "password"
 				},
 			},
 			ExpectedConf: `
-[transforms.loki_receiver_remap]
-type = "remap"
-inputs = ["application"]
-source = '''
-  del(.tag)
-'''
-
-[transforms.loki_receiver_dedot]
-type = "lua"
-inputs = ["loki_receiver_remap"]
-version = "2"
-hooks.init = "init"
-hooks.process = "process"
-source = '''
-    function init()
-        count = 0
-    end
-    function process(event, emit)
-        count = count + 1
-        event.log.openshift.sequence = count
-        if event.log.kubernetes == nil then
-            emit(event)
-            return
-        end
-        if event.log.kubernetes.labels == nil then
-            emit(event)
-            return
-        end
-		dedot(event.log.kubernetes.namespace_labels)
-        dedot(event.log.kubernetes.labels)
-        emit(event)
-    end
-
-    function dedot(map)
-        if map == nil then
-            return
-        end
-        local new_map = {}
-        local changed_keys = {}
-        for k, v in pairs(map) do
-            local dedotted = string.gsub(k, "[./]", "_")
-            if dedotted ~= k then
-                new_map[dedotted] = v
-                changed_keys[k] = true
-            end
-        end
-        for k in pairs(changed_keys) do
-            map[k] = nil
-        end
-        for k, v in pairs(new_map) do
-            map[k] = v
-        end
-    end
-'''
-
-[sinks.loki_receiver]
-type = "loki"
-inputs = ["loki_receiver_dedot"]
-endpoint = "https://logs-us-west1.grafana.net"
-out_of_order_action = "accept"
-healthcheck.enabled = false
-tenant_id = "{{foo.bar.baz}}"
-
-[sinks.loki_receiver.encoding]
-codec = "json"
-
-[sinks.loki_receiver.labels]
-kubernetes_container_name = "{{kubernetes.container_name}}"
-kubernetes_host = "${VECTOR_SELF_NODE_NAME}"
-kubernetes_namespace_name = "{{kubernetes.namespace_name}}"
-kubernetes_pod_name = "{{kubernetes.pod_name}}"
-log_type = "{{log_type}}"
-
-# Basic Auth Config
-[sinks.loki_receiver.auth]
-strategy = "basic"
-user = "username"
-password = "password"
-
-`,
+		[transforms.loki_receiver_remap]
+		type = "remap"
+		inputs = ["application"]
+		source = '''
+		 del(.tag)
+		'''
+		
+		[transforms.loki_receiver_dedot]
+		type = "lua"
+		inputs = ["loki_receiver_remap"]
+		version = "2"
+		hooks.init = "init"
+		hooks.process = "process"
+		source = '''
+		   function init()
+		       count = 0
+		   end
+		   function process(event, emit)
+		       count = count + 1
+		       event.log.openshift.sequence = count
+		       if event.log.kubernetes == nil then
+		           emit(event)
+		           return
+		       end
+		       if event.log.kubernetes.labels == nil then
+		           emit(event)
+		           return
+		       end
+				dedot(event.log.kubernetes.namespace_labels)
+		       dedot(event.log.kubernetes.labels)
+		       emit(event)
+		   end
+		
+		   function dedot(map)
+		       if map == nil then
+		           return
+		       end
+		       local new_map = {}
+		       local changed_keys = {}
+		       for k, v in pairs(map) do
+		           local dedotted = string.gsub(k, "[./]", "_")
+		           if dedotted ~= k then
+		               new_map[dedotted] = v
+		               changed_keys[k] = true
+		           end
+		       end
+		       for k in pairs(changed_keys) do
+		           map[k] = nil
+		       end
+		       for k, v in pairs(new_map) do
+		           map[k] = v
+		       end
+		   end
+		'''
+		
+		[sinks.loki_receiver]
+		type = "loki"
+		inputs = ["loki_receiver_dedot"]
+		endpoint = "https://logs-us-west1.grafana.net"
+		out_of_order_action = "accept"
+		healthcheck.enabled = false
+		tenant_id = "{{foo.bar.baz}}"
+		
+		[sinks.loki_receiver.encoding]
+		codec = "json"
+		
+		[sinks.loki_receiver.labels]
+		kubernetes_container_name = "{{kubernetes.container_name}}"
+		kubernetes_host = "${VECTOR_SELF_NODE_NAME}"
+		kubernetes_namespace_name = "{{kubernetes.namespace_name}}"
+		kubernetes_pod_name = "{{kubernetes.pod_name}}"
+		log_type = "{{log_type}}"
+		
+		# Basic Auth Config
+		[sinks.loki_receiver.auth]
+		strategy = "basic"
+		user = "username"
+		password = "password"
+		
+		`,
 		}),
 		Entry("with custom bearer token", helpers.ConfGenerateTest{
 			CLFSpec: logging.ClusterLogForwarderSpec{
@@ -374,6 +374,108 @@ password = "password"
 				"loki-receiver": {
 					Data: map[string][]byte{
 						"token": []byte("token-for-custom-loki"),
+					},
+				},
+			},
+			ExpectedConf: `
+		[transforms.loki_receiver_remap]
+		type = "remap"
+		inputs = ["application"]
+		source = '''
+		 del(.tag)
+		'''
+		
+		[transforms.loki_receiver_dedot]
+		type = "lua"
+		inputs = ["loki_receiver_remap"]
+		version = "2"
+		hooks.init = "init"
+		hooks.process = "process"
+		source = '''
+		   function init()
+		       count = 0
+		   end
+		   function process(event, emit)
+		       count = count + 1
+		       event.log.openshift.sequence = count
+		       if event.log.kubernetes == nil then
+		           emit(event)
+		           return
+		       end
+		       if event.log.kubernetes.labels == nil then
+		           emit(event)
+		           return
+		       end
+				dedot(event.log.kubernetes.namespace_labels)
+		       dedot(event.log.kubernetes.labels)
+		       emit(event)
+		   end
+		
+		   function dedot(map)
+		       if map == nil then
+		           return
+		       end
+		       local new_map = {}
+		       local changed_keys = {}
+		       for k, v in pairs(map) do
+		           local dedotted = string.gsub(k, "[./]", "_")
+		           if dedotted ~= k then
+		               new_map[dedotted] = v
+		               changed_keys[k] = true
+		           end
+		       end
+		       for k in pairs(changed_keys) do
+		           map[k] = nil
+		       end
+		       for k, v in pairs(new_map) do
+		           map[k] = v
+		       end
+		   end
+		'''
+		
+		[sinks.loki_receiver]
+		type = "loki"
+		inputs = ["loki_receiver_dedot"]
+		endpoint = "http://lokistack-dev-gateway-http.openshift-logging.svc:8080/api/logs/v1/application"
+		out_of_order_action = "accept"
+		healthcheck.enabled = false
+		
+		[sinks.loki_receiver.encoding]
+		codec = "json"
+		
+		[sinks.loki_receiver.labels]
+		kubernetes_container_name = "{{kubernetes.container_name}}"
+		kubernetes_host = "${VECTOR_SELF_NODE_NAME}"
+		kubernetes_namespace_name = "{{kubernetes.namespace_name}}"
+		kubernetes_pod_name = "{{kubernetes.pod_name}}"
+		log_type = "{{log_type}}"
+		
+		# Bearer Auth Config
+		[sinks.loki_receiver.auth]
+		strategy = "bearer"
+		token = "token-for-custom-loki"
+		`,
+		}),
+		Entry("with TLS insecureSkipVerify=true", helpers.ConfGenerateTest{
+			CLFSpec: logging.ClusterLogForwarderSpec{
+				Outputs: []logging.OutputSpec{
+					{
+						Type: logging.OutputTypeLoki,
+						Name: "loki-receiver",
+						URL:  "http://lokistack-dev-gateway-http.openshift-logging.svc:8080/api/logs/v1/application",
+						Secret: &logging.OutputSecretSpec{
+							Name: "custom-loki-secret",
+						},
+						TLS: &logging.OutputTLSSpec{
+							InsecureSkipVerify: true,
+						},
+					},
+				},
+			},
+			Secrets: map[string]*corev1.Secret{
+				"loki-receiver": {
+					Data: map[string][]byte{
+						"ca-bundle.crt": []byte("junk"),
 					},
 				},
 			},
@@ -450,10 +552,11 @@ kubernetes_namespace_name = "{{kubernetes.namespace_name}}"
 kubernetes_pod_name = "{{kubernetes.pod_name}}"
 log_type = "{{log_type}}"
 
-# Bearer Auth Config
-[sinks.loki_receiver.auth]
-strategy = "bearer"
-token = "token-for-custom-loki"
+[sinks.loki_receiver.tls]
+enabled = true
+verify_certificate = false
+verify_hostname = false
+ca_file = "/var/run/ocp-collector/secrets/custom-loki-secret/ca-bundle.crt"
 `,
 		}),
 	)
@@ -487,7 +590,7 @@ var _ = Describe("Generate vector config for in cluster loki", func() {
 type = "remap"
 inputs = ["application"]
 source = '''
-  del(.tag)
+ del(.tag)
 '''
 
 [transforms.loki_receiver_dedot]
@@ -497,45 +600,45 @@ version = "2"
 hooks.init = "init"
 hooks.process = "process"
 source = '''
-    function init()
-        count = 0
-    end
-    function process(event, emit)
-        count = count + 1
-        event.log.openshift.sequence = count
-        if event.log.kubernetes == nil then
-            emit(event)
-            return
-        end
-        if event.log.kubernetes.labels == nil then
-            emit(event)
-            return
-        end
+   function init()
+       count = 0
+   end
+   function process(event, emit)
+       count = count + 1
+       event.log.openshift.sequence = count
+       if event.log.kubernetes == nil then
+           emit(event)
+           return
+       end
+       if event.log.kubernetes.labels == nil then
+           emit(event)
+           return
+       end
 		dedot(event.log.kubernetes.namespace_labels)
-        dedot(event.log.kubernetes.labels)
-        emit(event)
-    end
+       dedot(event.log.kubernetes.labels)
+       emit(event)
+   end
 
-    function dedot(map)
-        if map == nil then
-            return
-        end
-        local new_map = {}
-        local changed_keys = {}
-        for k, v in pairs(map) do
-            local dedotted = string.gsub(k, "[./]", "_")
-            if dedotted ~= k then
-                new_map[dedotted] = v
-                changed_keys[k] = true
-            end
-        end
-        for k in pairs(changed_keys) do
-            map[k] = nil
-        end
-        for k, v in pairs(new_map) do
-            map[k] = v
-        end
-    end
+   function dedot(map)
+       if map == nil then
+           return
+       end
+       local new_map = {}
+       local changed_keys = {}
+       for k, v in pairs(map) do
+           local dedotted = string.gsub(k, "[./]", "_")
+           if dedotted ~= k then
+               new_map[dedotted] = v
+               changed_keys[k] = true
+           end
+       end
+       for k in pairs(changed_keys) do
+           map[k] = nil
+       end
+       for k, v in pairs(new_map) do
+           map[k] = v
+       end
+   end
 '''
 
 [sinks.loki_receiver]

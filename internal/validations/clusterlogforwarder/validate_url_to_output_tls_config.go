93f387c0c0efb97cf79fca43a1401aa1346a9263
Sergey Yedrikov <syedriko@redhat.com>
2023-05-26 22:27:58 -0400

    Make lint happy

--- internal/validations/clusterlogforwarder/validate_url_to_output_tls_config.go
+++ internal/validations/clusterlogforwarder/validate_url_to_output_tls_config.go
@@ -1,24 +1,24 @@
 package clusterlogforwarder
 
 import (
-  "fmt"
-  log "github.com/ViaQ/logerr/v2/log/static"
-  "github.com/openshift/cluster-logging-operator/apis/logging/v1"
-  "github.com/openshift/cluster-logging-operator/internal/url"
-  "strings"
+	"fmt"
+	log "github.com/ViaQ/logerr/v2/log/static"
+	"github.com/openshift/cluster-logging-operator/apis/logging/v1"
+	"github.com/openshift/cluster-logging-operator/internal/url"
+	"strings"
 )
 
 // validateUrlAccordingToTls validate that if Output has TLS configuration Output URL scheme must be secure e.g. https, tls etc
 func validateUrlAccordingToTls(clf v1.ClusterLogForwarder) error {
-  for i, output := range clf.Spec.Outputs {
-    _, output := i, output // Don't bind range variable.
-    u, _ := url.Parse(output.URL)
-    scheme := strings.ToLower(u.Scheme)
-    if !url.IsTLSScheme(scheme) && (output.TLS != nil && (output.TLS.InsecureSkipVerify || output.TLS.TLSSecurityProfile != nil)) {
-      log.V(3).Info("validateUrlAccordingToTls failed", "reason", "URL not secure but output has TLS configuration parameters",
-        "output URL", output.URL, "output Name", output.Name)
-      return fmt.Errorf("URL not secure: %v, but output %s has TLS configuration parameters", u, output.Name)
-    }
-  }
-  return nil
+	for i, output := range clf.Spec.Outputs {
+		_, output := i, output // Don't bind range variable.
+		u, _ := url.Parse(output.URL)
+		scheme := strings.ToLower(u.Scheme)
+		if !url.IsTLSScheme(scheme) && (output.TLS != nil && (output.TLS.InsecureSkipVerify || output.TLS.TLSSecurityProfile != nil)) {
+			log.V(3).Info("validateUrlAccordingToTls failed", "reason", "URL not secure but output has TLS configuration parameters",
+				"output URL", output.URL, "output Name", output.Name)
+			return fmt.Errorf("URL not secure: %v, but output %s has TLS configuration parameters", u, output.Name)
+		}
+	}
+	return nil
 }

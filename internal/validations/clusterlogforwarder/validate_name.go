b7a59ca7fd1d47a41feb2a31347aa39e4f60d241
Jeff Cantrill <jcantril@redhat.com>
2023-08-23 11:58:04 -0400

    LOG-4470: fix unallowed clf name

--- internal/validations/clusterlogforwarder/validate_name.go
+++ internal/validations/clusterlogforwarder/validate_name.go
@@ -4,6 +4,7 @@ import (
 	v1 "github.com/openshift/cluster-logging-operator/apis/logging/v1"
 	"github.com/openshift/cluster-logging-operator/internal/constants"
 	"github.com/openshift/cluster-logging-operator/internal/validations/errors"
+	"k8s.io/apimachinery/pkg/util/validation"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -12,5 +13,10 @@ func validateName(clf v1.ClusterLogForwarder, k8sClient client.Client, extras ma
 	if clf.Namespace == constants.OpenshiftNS && clf.Name == constants.CollectorName {
 		return errors.NewValidationError("Name %q conflicts with an object for the legacy ClusterLogForwarder deployment.  Choose another", clf.Name), nil
 	}
+
+	if nameErrors := validation.IsDNS1035Label(clf.Name); len(nameErrors) != 0 {
+		return errors.NewValidationError("Name %q will result in an invalid object: %v", clf.Name, nameErrors), nil
+	}
+
 	return nil, nil
 }

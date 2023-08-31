f39789cefda0c8cd6de8c5abc555ee5a1020e0eb
Jeff Cantrill <jcantril@redhat.com>
2023-08-31 13:57:10 -0400

    LOG-4178: fix vector normalization of EventRouter logs

--- test/functional/normalization/eventrouter_test.go
+++ test/functional/normalization/eventrouter_test.go
@@ -1,11 +1,11 @@
-//go:build fluentd
-
 package normalization
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/openshift/cluster-logging-operator/test/framework/functional"
 	testfw "github.com/openshift/cluster-logging-operator/test/functional"
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -21,64 +21,62 @@ import (
 	"k8s.io/client-go/tools/reference"
 )
 
-var _ = Describe("[Functional][Normalization] Fluentd normalization for EventRouter messages", func() {
+var _ = Describe("[Functional][Normalization] Messages from EventRouter", func() {
 
 	const timestamp string = "1985-10-21T09:00:00.00000+00:00"
 	var (
-		framework                *functional.CollectorFunctionalFramework
-		pod                      *corev1.Pod
-		nanoTime, _              = time.Parse(time.RFC3339Nano, timestamp)
-		templateForAnyKubernetes = types.Kubernetes{
-			ContainerName:    "*",
-			NamespaceName:    "*",
-			PodName:          "*",
-			ContainerImage:   "*",
-			ContainerImageID: "*",
-			PodID:            "*",
-			PodIP:            "**optional**",
-			Host:             "*",
-			MasterURL:        "*",
-			NamespaceID:      "*",
-			FlatLabels:       []string{"*"},
-			NamespaceLabels:  map[string]string{"*": "*"},
-		}
-		templateForAnyCollector = types.PipelineMetadata{
-			Collector: types.Collector{
-				Ipaddr4:    "*",
-				Inputname:  "*",
-				Name:       "*",
-				Version:    "*",
-				ReceivedAt: time.Time{},
-			},
+		framework                          *functional.CollectorFunctionalFramework
+		containerPath                      string
+		writeMsg                           func(msg string) error
+		templateForAnyKubernetesWithEvents = types.KubernetesWithEvent{
+			Kubernetes: functional.TemplateForAnyKubernetes,
 		}
 		NewEventDataBuilder = func(verb string, podRef *corev1.ObjectReference) types.EventData {
-			newEvent := types.NewMockEvent(podRef, corev1.EventTypeNormal, "reason", "message")
+			newEvent := types.NewEvent(podRef, corev1.EventTypeNormal, "reason", "amessage")
 			if verb == "UPDATED" {
-				oldEvent := types.NewMockEvent(podRef, corev1.EventTypeWarning, "old_reason", "old_message")
+				oldEvent := types.NewEvent(podRef, corev1.EventTypeWarning, "old_reason", "old_message")
 				return types.EventData{Verb: "UPDATED", Event: newEvent, OldEvent: oldEvent}
 			} else {
 				return types.EventData{Verb: "ADDED", Event: newEvent}
 			}
 		}
 
-		ExpectedLogTemplateBuilder = func(message string, timestamp time.Time) types.EventRouterLog {
-			return types.EventRouterLog{
-				Docker: types.Docker{
-					ContainerID: "*",
-				},
-				Kubernetes:       templateForAnyKubernetes,
-				Message:          message,
-				Level:            "*",
-				Hostname:         "*",
-				PipelineMetadata: templateForAnyCollector,
-				Timestamp:        timestamp,
-				LogType:          "application",
-				ViaqMsgID:        "*",
-				OpenshiftLabels: types.OpenshiftMeta{
-					ClusterID: "*",
-					Sequence:  types.NewOptionalInt(""),
+		ExpectedLogTemplateBuilder = func(event, oldEvent *corev1.Event) types.EventRouterLog {
+			tmpl := types.EventRouterLog{
+				Kubernetes: templateForAnyKubernetesWithEvents,
+				ViaQCommon: types.ViaQCommon{
+					Message:          event.Message,
+					Level:            types.AnyString,
+					Hostname:         types.AnyString,
+					PipelineMetadata: functional.TemplateForAnyPipelineMetadata,
+					Timestamp:        time.Time{},
+					LogType:          logging.InputNameApplication,
+					ViaqMsgID:        types.AnyString,
+					Openshift: types.OpenshiftMeta{
+						ClusterID: types.AnyString,
+						Sequence:  types.NewOptionalInt(""),
+					},
 				},
 			}
+			//optional for test given we are mocking and these values may not map to actual meta
+			tmpl.Kubernetes.ContainerImage = types.OptionalString
+			tmpl.Kubernetes.ContainerImageID = types.OptionalString
+			tmpl.Kubernetes.PodID = types.OptionalString
+			tmpl.PipelineMetadata.Collector.OriginalRawMessage = types.AnyString
+			tmpl.Kubernetes.Event = types.ViaqEventRouterEvent{
+				Event: *event,
+				Verb:  types.AnyString,
+			}
+			tmpl.Kubernetes.Event.Event.Message = ""
+			if oldEvent != nil {
+				tmpl.OldEvent = oldEvent
+			}
+			if testfw.LogCollectionType == logging.LogCollectionTypeVector {
+				tmpl.ViaQCommon.PipelineMetadata = types.PipelineMetadata{}
+				tmpl.ViaQCommon.ViaqMsgID = types.OptionalString
+			}
+
+			return tmpl
 		}
 	)
 
@@ -86,35 +84,52 @@ var _ = Describe("[Functional][Normalization] Fluentd normalization for EventRou
 		framework = functional.NewCollectorFunctionalFrameworkUsingCollector(testfw.LogCollectionType)
 		functional.NewClusterLogForwarderBuilder(framework.Forwarder).
 			FromInput(logging.InputNameApplication).
-			ToFluentForwardOutput()
+			ToHttpOutput()
+
+		if testfw.LogCollectionType == logging.LogCollectionTypeFluentd {
+			containerPath = fmt.Sprintf("/var/log/pods/%s_eventrouter-123_12345/eventrouter/0.log", framework.Namespace)
+			framework.VisitConfig = func(conf string) string {
+				return strings.Replace(conf, "/var/log/pods/*/*/*.log", containerPath, 1)
+			}
+			writeMsg = func(msg string) error {
+				return framework.WriteMessagesToLog(msg, 1, containerPath)
+			}
+		} else {
+			// vector only collects logs using pods, namespaces, containers it knows about.
+			writeMsg = func(msg string) error {
+				return framework.WriteMessagesToApplicationLog(msg, 1)
+			}
+			framework.VisitConfig = func(conf string) string {
+				return strings.Replace(conf, `pod_name, "event-router-"`, `pod_name, "functional"`, 1)
+			}
+		}
 		Expect(framework.Deploy()).To(BeNil())
-		pod = types.NewMockPod()
+
 	})
 	AfterEach(func() {
 		framework.Cleanup()
 	})
 
-	DescribeTable("when normalizing events", func(verb string) {
-		podRef, err := reference.GetReference(scheme.Scheme, pod)
+	DescribeTable("should be normalized to the VIAQ data model", func(verb string) {
+		podRef, err := reference.GetReference(scheme.Scheme, types.NewMockPod())
 		Expect(err).To(BeNil())
 		newEventData := NewEventDataBuilder(verb, podRef)
 		jsonBytes, _ := json.Marshal(newEventData)
 		jsonStr := string(jsonBytes)
 		msg := functional.NewCRIOLogMessage(timestamp, jsonStr, false)
-		err = framework.WriteMessagesToApplicationLog(msg, 1)
+		err = writeMsg(msg)
 		Expect(err).To(BeNil())
 
-		raw, err := framework.ReadRawApplicationLogsFrom(logging.OutputTypeFluentdForward)
+		raw, err := framework.ReadRawApplicationLogsFrom(logging.OutputTypeHttp)
 		Expect(err).To(BeNil(), "Expected no errors reading the logs")
 		var logs []types.EventRouterLog
 		err = types.StrictlyParseLogs(utils.ToJsonLogs(raw), &logs)
 		Expect(err).To(BeNil(), "Expected no errors parsing the logs")
-		var expectedLogTemplate = ExpectedLogTemplateBuilder(jsonStr, nanoTime)
-		outputTestLog := logs[0]
-		Expect(outputTestLog).To(matchers.FitLogFormatTemplate(expectedLogTemplate))
+		var expectedLogTemplate = ExpectedLogTemplateBuilder(newEventData.Event, newEventData.OldEvent)
+		Expect(logs[0]).To(matchers.FitLogFormatTemplate(expectedLogTemplate))
 	},
-		Entry("It should normalize 'ADDED' events", "ADDED"),
-		Entry("It should normalize 'UPDATED' events", "UPDATED"),
+		Entry("for ADDED events", "ADDED"),
+		Entry("for UPDATED events", "UPDATED"),
 	)
 
 })

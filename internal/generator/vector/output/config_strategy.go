de3c4b273b30dbbfc74c2d257054f3a19a001c30
Jeff Cantrill <jcantril@redhat.com>
2024-03-20 13:37:55 -0400

    LOG-5166: Validate allocated buffer for CLF

--- internal/generator/vector/output/config_strategy.go
+++ internal/generator/vector/output/config_strategy.go
@@ -7,11 +7,6 @@ import (
 	"github.com/openshift/cluster-logging-operator/internal/generator/vector/output/common"
 )
 
-const (
-	minBufferSize   = 268435488
-	buffertTypeDisk = "disk"
-)
-
 func (o Output) VisitSink(s common.SinkConfig) {
 	if o.spec.Tuning != nil {
 		comp := o.spec.Tuning.Compression
@@ -56,8 +51,8 @@ func (o Output) VisitBuffer(b common.Buffer) common.Buffer {
 		switch o.spec.Tuning.Delivery {
 		case logging.OutputDeliveryModeAtLeastOnce:
 			b.WhenFull.Value = common.BufferWhenFullBlock
-			b.Type.Value = buffertTypeDisk
-			b.MaxSize.Value = minBufferSize
+			b.Type.Value = common.BufferTypeDisk
+			b.MaxSize.Value = common.BufferMinSizeBytes
 		case logging.OutputDeliveryModeAtMostOnce:
 			b.WhenFull.Value = common.BufferWhenFullDropNewest
 		}

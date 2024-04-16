fd37d9a43d93816d7232d4b435095627f7881e8d
Vitalii Parfonov <vparfono@redhat.com>
2024-04-16 19:42:09 +0300

    LOG-4672: ensure timestamp field has correct format for Splunk
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- test/functional/outputs/splunk/forward_to_splunk_test.go
+++ test/functional/outputs/splunk/forward_to_splunk_test.go
@@ -67,6 +67,44 @@ var _ = Describe("Forwarding to Splunk", func() {
 		Expect(outputTestLog.LogType).To(Equal(logging.InputNameApplication))
 	})
 
+	It("should accept audit logs without timestamp unexpected type warning (see: https://issues.redhat.com/browse/LOG-4672)", func() {
+		functional.NewClusterLogForwarderBuilder(framework.Forwarder).
+			FromInput(logging.InputNameAudit).
+			ToSplunkOutput()
+		framework.Secrets = append(framework.Secrets, secret)
+		Expect(framework.Deploy()).To(BeNil())
+
+		// Wait for splunk to be ready
+		time.Sleep(90 * time.Second)
+
+		// Write audit logs
+		timestamp, _ := time.Parse(time.RFC3339Nano, "2024-04-16T09:46:19.116+00:00")
+		auditLogLine := functional.NewAuditHostLog(timestamp)
+		writeAuditLogs := framework.WriteMessagesToAuditLog(auditLogLine, 1)
+		Expect(writeAuditLogs).To(BeNil(), "Expect no errors writing audit logs")
+
+		// Read audit logs
+		logs, err := framework.ReadLogsByTypeFromSplunk(framework.Namespace, framework.Name, logging.InputNameAudit)
+		Expect(err).To(BeNil(), "Expected no errors getting logs from splunk")
+		Expect(logs).ToNot(BeEmpty())
+
+		// Parse the logs
+		var auditLogs []types.AuditLog
+		jsonString := fmt.Sprintf("[%s]", strings.Join(logs, ","))
+		err = types.ParseLogsFrom(jsonString, &auditLogs, false)
+		Expect(err).To(BeNil(), "Expected no errors parsing the logs")
+		Expect(len(auditLogs)).To(Equal(1), "Expected one audit log")
+
+		Expect(auditLogs[0].LogType).To(Equal(logging.InputNameAudit), "Expected audit log type")
+		Expect(auditLogs[0].Level).To(Equal("default"), "Expected audit log level to default")
+
+		collectorLog, err := framework.ReadCollectorLogs()
+		Expect(err).To(BeNil(), "Expected no errors reading the collector logs")
+		Expect(collectorLog).ToNot(BeEmpty(), "Expected collector logs to not be empty")
+		tsWarn := "Timestamp was an unexpected type. Deferring to Splunk to set the timestamp"
+		Expect(strings.Contains(collectorLog, tsWarn)).To(BeFalse(), "Expected collector logs to NOT contain timestamp unexpected type warning")
+	})
+
 	Context("with custom indexes", func() {
 		withIndexName := func(spec *logging.OutputSpec) {
 			spec.Splunk = &logging.Splunk{

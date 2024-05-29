fbc624f85f43959a7a975e56fde502aec4eef068
Vitalii Parfonov <vparfono@redhat.com>
2024-05-29 19:20:00 +0300

    Fix NPE is output type spec not declared
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- internal/api/observability/tuning.go
+++ internal/api/observability/tuning.go
@@ -13,40 +13,40 @@ func NewTuning(spec obs.OutputSpec) Tuning {
 	t := Tuning{}
 	switch spec.Type {
 	case obs.OutputTypeAzureMonitor:
-		if spec.Cloudwatch.Tuning != nil {
+		if spec.AzureMonitor != nil && spec.AzureMonitor.Tuning != nil {
 			t.BaseOutputTuningSpec = *spec.AzureMonitor.Tuning
 		}
 	case obs.OutputTypeCloudwatch:
-		if spec.Cloudwatch.Tuning != nil {
+		if spec.Cloudwatch != nil && spec.Cloudwatch.Tuning != nil {
 			t.BaseOutputTuningSpec = spec.Cloudwatch.Tuning.BaseOutputTuningSpec
 			t.Compression = spec.Cloudwatch.Tuning.Compression
 		}
 	case obs.OutputTypeElasticsearch:
-		if spec.Elasticsearch.Tuning != nil {
+		if spec.Elasticsearch != nil && spec.Elasticsearch.Tuning != nil {
 			t.BaseOutputTuningSpec = spec.Elasticsearch.Tuning.BaseOutputTuningSpec
 			t.Compression = spec.Elasticsearch.Tuning.Compression
 		}
 	case obs.OutputTypeHTTP:
-		if spec.HTTP.Tuning != nil {
+		if spec.HTTP != nil && spec.HTTP.Tuning != nil {
 			t.BaseOutputTuningSpec = spec.HTTP.Tuning.BaseOutputTuningSpec
 			t.Compression = spec.HTTP.Tuning.Compression
 		}
 	case obs.OutputTypeKafka:
-		if spec.Kafka.Tuning != nil {
+		if spec.Kafka != nil && spec.Kafka.Tuning != nil {
 			t.Compression = spec.Kafka.Tuning.Compression
 		}
 	case obs.OutputTypeLoki:
-		if spec.Loki.Tuning != nil {
+		if spec.Loki != nil && spec.Loki.Tuning != nil {
 			t.BaseOutputTuningSpec = spec.Loki.Tuning.BaseOutputTuningSpec
 			t.Compression = spec.Loki.Tuning.Compression
 		}
 	case obs.OutputTypeLokiStack:
-		if spec.LokiStack.Tuning != nil {
+		if spec.LokiStack != nil && spec.LokiStack.Tuning != nil {
 			t.BaseOutputTuningSpec = spec.LokiStack.Tuning.BaseOutputTuningSpec
 			t.Compression = spec.LokiStack.Tuning.Compression
 		}
 	case obs.OutputTypeSplunk:
-		if spec.Splunk.Tuning != nil {
+		if spec.Splunk != nil && spec.Splunk.Tuning != nil {
 			t.BaseOutputTuningSpec = spec.Splunk.Tuning.BaseOutputTuningSpec
 		}
 	}

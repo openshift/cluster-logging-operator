189dc1d7295deb0bc9859cb41244b6400bf26700
Vitalii Parfonov <vparfono@redhat.com>
2024-05-31 14:18:59 +0300

    Fix paths for volume mounts
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- internal/collector/common/path.go
+++ internal/collector/common/path.go
@@ -1,26 +1,25 @@
 package common
 
 import (
-	"fmt"
 	"path/filepath"
 )
 
 // SecretPath is the path for any secret visible to the collector
 func SecretPath(secretName string, file string) string {
-	return fmt.Sprintf("%q", filepath.Join("/var/run/ocp-collector/secrets", secretName, file))
+	return filepath.Join("/var/run/ocp-collector/secrets", secretName, file)
 }
 
 // SecretBasePath is the path for any secret visible to the collector
 func SecretBasePath(secretName string) string {
-	return fmt.Sprintf("%q", filepath.Join("/var/run/ocp-collector/secrets", secretName))
+	return filepath.Join("/var/run/ocp-collector/secrets", secretName)
 }
 
 // ConfigmapPath is the path for any configmap visible to the collector
 func ConfigmapPath(name string, file string) string {
-	return fmt.Sprintf("%q", filepath.Join("/var/run/ocp-collector/config", name, file))
+	return filepath.Join("/var/run/ocp-collector/config", name, file)
 }
 
 // ConfigmapBasePath is the path for any configmap visible to the collector
 func ConfigmapBasePath(name string) string {
-	return fmt.Sprintf("%q", filepath.Join("/var/run/ocp-collector/config", name))
+	return filepath.Join("/var/run/ocp-collector/config", name)
 }

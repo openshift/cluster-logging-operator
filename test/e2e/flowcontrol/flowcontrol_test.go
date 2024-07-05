f51a2f8c1c4fc658c6001fa16b05a0b9327c3a19
Casey Hartman <cahartma@redhat.com>
2024-07-05 09:00:40 -0400

    Updated based on latest discussion and test changes

--- test/e2e/flowcontrol/flowcontrol_test.go
+++ test/e2e/flowcontrol/flowcontrol_test.go
@@ -1,36 +1,37 @@
 package flowcontrol
 
 import (
-	"context"
 	"fmt"
+	"github.com/openshift/cluster-logging-operator/internal/constants"
+	"github.com/openshift/cluster-logging-operator/test/runtime"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	loggingv1 "github.com/openshift/cluster-logging-operator/api/logging/v1"
+	obs "github.com/openshift/cluster-logging-operator/api/observability/v1"
+	internalruntime "github.com/openshift/cluster-logging-operator/internal/runtime"
+	obsruntime "github.com/openshift/cluster-logging-operator/internal/runtime/observability"
 	"github.com/openshift/cluster-logging-operator/test/client"
 	framework "github.com/openshift/cluster-logging-operator/test/framework/e2e"
-	"github.com/openshift/cluster-logging-operator/test/helpers"
 	"github.com/openshift/cluster-logging-operator/test/helpers/loki"
-	"github.com/openshift/cluster-logging-operator/test/runtime"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("[E2E] FlowControl", func() {
 
 	var (
-		e2e          *framework.E2ETestFramework
-		cl           *loggingv1.ClusterLogging
-		clf          *loggingv1.ClusterLogForwarder
-		l            *loki.Receiver
-		logStressors []corev1.Pod
-		lokiNS       string
-		stressorNS   string
-		promQuery    string
-	)
+		e2e           *framework.E2ETestFramework
+		clf           *obs.ClusterLogForwarder
+		l             *loki.Receiver
+		logStressors  []corev1.Pod
+		lokiNS        string
+		stressorNS    string
+		promQuery     string
+		forwarderName = "collector"
 
-	components := []helpers.LogComponentType{helpers.ComponentTypeCollectorVector, helpers.ComponentTypeStore}
+		sa  *corev1.ServiceAccount
+		err error
+	)
 
 	DeployLoggingComponents := func() {
 		for idx := range logStressors {
@@ -39,44 +40,31 @@ var _ = Describe("[E2E] FlowControl", func() {
 
 		Expect(l.Create(client.Get())).To(Succeed())
 
-		if err := e2e.CreateClusterLogging(cl); err != nil {
-			Fail(fmt.Sprintf("Unable to create an instance of cluster logging: %v", err))
-		}
-
-		if err := e2e.CreateClusterLogForwarder(clf); err != nil {
+		if err := e2e.CreateObservabilityClusterLogForwarder(clf); err != nil {
 			Fail(fmt.Sprintf("Unable to create an instance of clusterlogforwarder: %v", err))
 		}
 
-		for _, component := range components {
-			if err := e2e.WaitFor(component); err != nil {
-				Fail(fmt.Sprintf("Failed waiting for component %s to be ready: %v", component, err))
-			}
+		if err := e2e.WaitForDaemonSet(clf.Namespace, clf.Name); err != nil {
+			Fail(fmt.Sprintf("Failed waiting for collector %s/%s to be ready: %v", clf.Namespace, clf.Name, err))
 		}
 	}
 
 	BeforeEach(func() {
-		//TODO: fix me
-		Skip("fix me after  new API")
 		logStressors = make([]corev1.Pod, 0)
 		e2e = framework.NewE2ETestFramework()
-
-		cl = helpers.NewClusterLogging(components...)
 
 		lokiNS = e2e.CreateTestNamespace()
 		stressorNS = e2e.CreateTestNamespace()
 
-		e2e.AddCleanup(func() error {
-			opts := metav1.DeleteOptions{}
-			return e2e.KubeClient.CoreV1().Namespaces().Delete(context.TODO(), lokiNS, opts)
-		})
-
-		e2e.AddCleanup(func() error {
-			opts := metav1.DeleteOptions{}
-			return e2e.KubeClient.CoreV1().Namespaces().Delete(context.TODO(), stressorNS, opts)
-		})
+		sa, err = e2e.BuildAuthorizationFor(constants.OpenshiftNS, forwarderName).
+			AllowClusterRole(framework.ClusterRoleCollectApplicationLogs).
+			Create()
+		Expect(err).To(BeNil())
 
 		l = loki.NewReceiver(lokiNS, "loki-server")
-		clf = runtime.NewClusterLogForwarder()
+		clf = obsruntime.NewClusterLogForwarder(constants.OpenshiftNS, forwarderName, internalruntime.Initialize, func(clf *obs.ClusterLogForwarder) {
+			clf.Spec.ServiceAccount.Name = sa.Name
+		})
 
 		for i := 0; i < 10; i += 1 {
 			logStressors = append(logStressors,
@@ -89,114 +77,35 @@ var _ = Describe("[E2E] FlowControl", func() {
 		e2e.Cleanup()
 	}, framework.DefaultCleanUpTimeout)
 
-	It("applying policies in input/output of pipeline", func() {
-		clf.Spec.Inputs = []loggingv1.InputSpec{
-			{
-				Name: "custom-app-0",
-				Application: &loggingv1.Application{
-					Namespaces: []string{stressorNS},
-					ContainerLimit: &loggingv1.LimitSpec{
-						MaxRecordsPerSecond: 50,
-					}, // 10 files and 100 group limit, so 10 lines per file,
-				},
-			},
-			{
-				Name: "custom-app-1",
-				Application: &loggingv1.Application{
-					Namespaces: []string{stressorNS},
-					ContainerLimit: &loggingv1.LimitSpec{
-						MaxRecordsPerSecond: 50,
-					}, // 10 files and 100 group limit, so 10 lines per file,
-				},
-			},
-		}
-		clf.Spec.Outputs = []loggingv1.OutputSpec{
-			{
-				Name: "loki-0",
-				Type: loggingv1.OutputTypeLoki,
-				URL:  l.InternalURL("").String(),
-				OutputTypeSpec: loggingv1.OutputTypeSpec{
-					Loki: &loggingv1.Loki{},
-				},
-			},
-			{
-				Name: "loki-1",
-				Type: loggingv1.OutputTypeLoki,
-				URL:  l.InternalURL("").String(),
-				OutputTypeSpec: loggingv1.OutputTypeSpec{
-					Loki: &loggingv1.Loki{},
-				},
-				Limit: &loggingv1.LimitSpec{
-					MaxRecordsPerSecond: 100,
-				},
-			},
-		}
-		clf.Spec.Pipelines = []loggingv1.PipelineSpec{
-			{
-				InputRefs:  []string{"custom-app-0"},
-				OutputRefs: []string{"loki-0"},
-				Name:       "group-policy-at-application",
-			},
-			{
-				InputRefs:  []string{"custom-app-1"},
-				OutputRefs: []string{"loki-0"},
-				Name:       "container-policy-at-application",
-			},
-			{
-				InputRefs:  []string{loggingv1.InputNameApplication},
-				OutputRefs: []string{"loki-1"},
-				Name:       "policy-at-loki",
-			},
-		}
-
-		DeployLoggingComponents()
-
-		if WaitForMetricsToShow() == false {
-			Fail("Metrics not showing up in Prometheus")
-		}
-		// sleeping for 1 minute to ensure rate of metrics is stable
-		time.Sleep(30 * time.Second)
-
-		promQuery = fmt.Sprintf(VectorCompSentEvents, "group-policy-at-application")
-		ExpectMetricsWithinRange(GetCollectorMetrics(promQuery), 0, 0)
-
-		promQuery = fmt.Sprintf(SumMetric, VectorCompSentEvents)
-		promQuery = fmt.Sprintf(promQuery, "container-policy-at-application") // Max number of logs allowed per second is 10 * 100 lines/sec
-		ExpectMetricsWithinRange(GetCollectorMetrics(promQuery), 100, 1002)
-
-		promQuery = fmt.Sprintf(VectorCompSentEvents, "loki-1")
-		ExpectMetricsWithinRange(GetCollectorMetrics(promQuery), 0, 102)
-	})
-
-	It("applying policies in both input and output", func() {
-		clf.Spec.Inputs = []loggingv1.InputSpec{
+	It("should apply policies in both input and output", func() {
+		clf.Spec.Inputs = []obs.InputSpec{
 			{
 				Name: "custom-app-2",
-				Application: &loggingv1.Application{
-					//LOG-4362
-					//GroupLimit: &loggingv1.LimitSpec{
-					//	MaxRecordsPerSecond: 200,
-					//},
-					ContainerLimit: &loggingv1.LimitSpec{
-						MaxRecordsPerSecond: 200,
+				Type: obs.InputTypeApplication,
+				Application: &obs.Application{
+					Tuning: &obs.ContainerInputTuningSpec{
+						RateLimitPerContainer: &obs.LimitSpec{
+							MaxRecordsPerSecond: 200,
+						},
 					},
 				},
 			},
 		}
-		clf.Spec.Outputs = []loggingv1.OutputSpec{
+		clf.Spec.Outputs = []obs.OutputSpec{
 			{
 				Name: "loki-2",
-				Type: loggingv1.OutputTypeLoki,
-				URL:  l.InternalURL("").String(),
-				OutputTypeSpec: loggingv1.OutputTypeSpec{
-					Loki: &loggingv1.Loki{},
+				Type: obs.OutputTypeLoki,
+				Loki: &obs.Loki{
+					URLSpec: obs.URLSpec{
+						URL: l.InternalURL("").String(),
+					},
 				},
-				Limit: &loggingv1.LimitSpec{
+				Limit: &obs.LimitSpec{
 					MaxRecordsPerSecond: 100,
 				},
 			},
 		}
-		clf.Spec.Pipelines = []loggingv1.PipelineSpec{
+		clf.Spec.Pipelines = []obs.PipelineSpec{
 			{
 				InputRefs:  []string{"custom-app-2"},
 				OutputRefs: []string{"loki-2"},

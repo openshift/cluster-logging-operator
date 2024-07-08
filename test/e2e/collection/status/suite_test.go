1d39f31b99351f59fbc01ce6db4b6f36f0df44b5
Jeff Cantrill <jcantril@redhat.com>
2024-07-08 17:17:52 -0400

    LOG-5784: Enable the e2e misc status test

---
+++ test/e2e/collection/status/suite_test.go
@@ -0,0 +1,13 @@
+package status
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "[e2e][collection][status] Suite")
+}

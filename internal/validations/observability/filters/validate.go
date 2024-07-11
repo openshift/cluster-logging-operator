9242a8d105d6aa946013da4afe8acd037f84926d
Jeff Cantrill <jcantril@redhat.com>
2024-07-11 17:20:15 -0400

    LOG-5788: fix enabling filters

---
+++ internal/validations/observability/filters/validate.go
@@ -0,0 +1,16 @@
+package filters
+
+import (
+	obs "github.com/openshift/cluster-logging-operator/api/observability/v1"
+	internalcontext "github.com/openshift/cluster-logging-operator/internal/api/context"
+	internalobs "github.com/openshift/cluster-logging-operator/internal/api/observability"
+)
+
+func Validate(context internalcontext.ForwarderContext) {
+	filterMap := internalobs.FilterMap(context.Forwarder.Spec)
+	for _, filter := range filterMap {
+		internalobs.SetCondition(&context.Forwarder.Status.Filters,
+			internalobs.NewConditionFromPrefix(obs.ConditionTypeValidFilterPrefix, filter.Name, true, obs.ReasonValidationSuccess, "filter valid"))
+	}
+
+}

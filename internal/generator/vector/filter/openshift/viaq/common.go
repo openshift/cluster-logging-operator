e8fc180bab6351f1d9dea836fe58ddd8d31d7436
Casey Hartman <cahartma@redhat.com>
2024-07-13 17:18:10 -0400

    Add .file to the internal context object

--- internal/generator/vector/filter/openshift/viaq/common.go
+++ internal/generator/vector/filter/openshift/viaq/common.go
@@ -3,5 +3,8 @@ package viaq
 const (
 	ClusterID         = `.openshift.cluster_id = "${OPENSHIFT_CLUSTER_ID:-}"`
 	FixTimestampField = `ts = del(.timestamp); if !exists(."@timestamp") {."@timestamp" = ts}`
-	InternalContext   = `._internal.message = .message`
+	InternalContext   = `
+._internal.message = .message
+._internal.file = .file
+`
 )

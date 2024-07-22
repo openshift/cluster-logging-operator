e9d63b44062eb7be7608141a4f25636f5a778fa0
Jeff Cantrill <jcantril@redhat.com>
2024-07-22 14:22:47 -0400

    LOG-5844: Fix reference docs to be for observability api

---
+++ api/observability/v1/doc.go
@@ -0,0 +1,8 @@
+// Package v1 contains API Schema definitions for the observability.openshift.io group to support
+// logging
+//
+// +k8s:deepcopy-gen=package,register
+// +groupName=observability.openshift.io
+// +k8s:openapi-gen=true
+// +kubebuilder:object:generate=true
+package v1

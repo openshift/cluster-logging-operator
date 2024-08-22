aea7a99fd0fd9c5093c36792886e222bb588fb1b
Robert Jacob <rojacob@redhat.com>
2024-08-22 20:29:47 +0200

    LOG-5993: Sort names of secrets / configmaps so that order is stable

--- internal/api/observability/configmap.go
+++ internal/api/observability/configmap.go
@@ -1,6 +1,10 @@
 package observability
 
-import corev1 "k8s.io/api/core/v1"
+import (
+	"sort"
+
+	corev1 "k8s.io/api/core/v1"
+)
 
 type ConfigMaps map[string]*corev1.ConfigMap
 
@@ -8,5 +12,7 @@ func (c ConfigMaps) Names() (names []string) {
 	for name := range c {
 		names = append(names, name)
 	}
+
+	sort.Strings(names)
 	return names
 }

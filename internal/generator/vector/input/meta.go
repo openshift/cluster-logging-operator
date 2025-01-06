3584b386072f679952f7f22185961632ba4c06c8
Calvin Lee <calee@redhat.com>
2025-01-06 12:01:16 -0500

    LOG-6373: Port LOG-6354 to release-6.1

--- internal/generator/vector/input/meta.go
+++ internal/generator/vector/input/meta.go
@@ -2,15 +2,21 @@ package input
 
 import (
 	"fmt"
+
 	"github.com/openshift/cluster-logging-operator/internal/generator/framework"
 	"github.com/openshift/cluster-logging-operator/internal/generator/vector/elements"
 	"github.com/openshift/cluster-logging-operator/internal/generator/vector/helpers"
 )
 
-func NewLogSourceAndType(id string, logSource, logType interface{}, inputs string) framework.Element {
-	return elements.Remap{
+func NewLogSourceAndType(id string, logSource, logType interface{}, inputs string, visit func(remap *elements.Remap)) framework.Element {
+	ele := elements.Remap{
 		ComponentID: id,
 		Inputs:      helpers.MakeInputs(inputs),
 		VRL:         fmt.Sprintf(".log_source = %q\n.log_type = %q", logSource, logType),
 	}
+
+	if visit != nil {
+		visit(&ele)
+	}
+	return ele
 }

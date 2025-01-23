ab6a3efdf5c1619d3eda0a1271b7b43a9a05486c
Calvin Lee <calee@redhat.com>
2025-01-23 17:37:02 -0500

    LOG-5110: Refactor comparators with controllerutil's CreateOrUpdate function

--- internal/reconcile/rbac.go
+++ internal/reconcile/rbac.go
@@ -6,125 +6,56 @@ import (
 
 	log "github.com/ViaQ/logerr/v2/log/static"
 	"github.com/openshift/cluster-logging-operator/internal/runtime"
-	"github.com/openshift/cluster-logging-operator/internal/utils"
-	"github.com/openshift/cluster-logging-operator/internal/utils/comparators/clusterrole"
-	"github.com/openshift/cluster-logging-operator/internal/utils/comparators/clusterrolebinding"
-	"github.com/openshift/cluster-logging-operator/internal/utils/comparators/role"
-	"github.com/openshift/cluster-logging-operator/internal/utils/comparators/rolebinding"
 	rbacv1 "k8s.io/api/rbac/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/client-go/util/retry"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 func Role(k8Client client.Client, desired *rbacv1.Role) error {
-
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		current := &rbacv1.Role{}
-		key := client.ObjectKeyFromObject(desired)
-		if err := k8Client.Get(context.TODO(), key, current); err != nil {
-			if apierrors.IsNotFound(err) {
-				return k8Client.Create(context.TODO(), desired)
-			}
-			return fmt.Errorf("failed to get %v role: %w", key, err)
-		}
-		if role.AreSame(current, desired) && utils.HasSameOwner(current.OwnerReferences, desired.OwnerReferences) {
-			log.V(3).Info("Roles are the same skipping update")
-			return nil
-		}
-		current.Rules = desired.Rules
-		current.OwnerReferences = desired.OwnerReferences
-
-		return k8Client.Update(context.TODO(), current)
+	role := runtime.NewRole(desired.Namespace, desired.Name)
+	op, err := controllerutil.CreateOrUpdate(context.TODO(), k8Client, role, func() error {
+		// Update the role with our desired state
+		role.Rules = desired.Rules
+		role.OwnerReferences = desired.OwnerReferences
+		return nil
 	})
-	return retryErr
+
+	if err == nil {
+		log.V(3).Info(fmt.Sprintf("reconciled role - operation: %s", op))
+	}
+	return err
 }
 
 func RoleBinding(k8Client client.Client, desired *rbacv1.RoleBinding) error {
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		current := &rbacv1.RoleBinding{}
-		key := client.ObjectKeyFromObject(desired)
-		if err := k8Client.Get(context.TODO(), key, current); err != nil {
-			if apierrors.IsNotFound(err) {
-				return k8Client.Create(context.TODO(), desired)
-			}
-			return fmt.Errorf("failed to get %v rolebinding: %w", key, err)
-		}
-		if rolebinding.AreSame(current, desired) && utils.HasSameOwner(current.OwnerReferences, desired.OwnerReferences) {
-			log.V(3).Info("RoleBindings are the same skipping update")
-			return nil
-		}
-		current.RoleRef = desired.RoleRef
-		current.Subjects = desired.Subjects
-		current.OwnerReferences = desired.OwnerReferences
-
-		return k8Client.Update(context.TODO(), current)
-	})
-	return retryErr
-}
-
-func ClusterRole(k8sClient client.Client, name string, generator func() *rbacv1.ClusterRole) (modified bool, err error) {
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		current := &rbacv1.ClusterRole{}
-		key := client.ObjectKey{Name: name}
-		if err := k8sClient.Get(context.TODO(), key, current); err != nil {
-			if !apierrors.IsNotFound(err) {
-				return fmt.Errorf("failed to get ClusterRole: %w", err)
-			}
-
-			current = generator()
-			if err := k8sClient.Create(context.TODO(), current); err != nil {
-				return fmt.Errorf("failed to create ClusterRole: %w", err)
-			}
-
-			modified = true
-			return nil
-		}
-
-		wantRole := generator()
-		if clusterrole.AreSame(current, wantRole) {
-			log.V(9).Info("ClusterRole matches.")
-			return nil
-		}
-
-		current.Rules = wantRole.Rules
-
-		modified = true
-		return k8sClient.Update(context.TODO(), current)
+	roleBinding := runtime.NewRoleBinding(desired.Namespace, desired.Name, desired.RoleRef)
+	op, err := controllerutil.CreateOrUpdate(context.TODO(), k8Client, roleBinding, func() error {
+		// Update the rolebinding with our desired state
+		roleBinding.RoleRef = desired.RoleRef
+		roleBinding.Subjects = desired.Subjects
+		roleBinding.OwnerReferences = desired.OwnerReferences
+		return nil
 	})
 
-	return modified, retryErr
+	if err == nil {
+		log.V(3).Info(fmt.Sprintf("reconciled roleBinding - operation: %s", op))
+	}
+	return err
 }
 
 func ClusterRoleBinding(k8sClient client.Client, name string, generator func() *rbacv1.ClusterRoleBinding) error {
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		current := &rbacv1.ClusterRoleBinding{}
-		key := client.ObjectKey{Name: name}
-		if err := k8sClient.Get(context.TODO(), key, current); err != nil {
-			if !apierrors.IsNotFound(err) {
-				return fmt.Errorf("failed to get ClusterRoleBinding: %w", err)
-			}
-
-			current = generator()
-			if err := k8sClient.Create(context.TODO(), current); err != nil {
-				return fmt.Errorf("failed to create ClusterRoleBinding: %w", err)
-			}
-
-			return nil
-		}
-
-		wantRoleBinding := generator()
-		if clusterrolebinding.AreSame(current, wantRoleBinding) {
-			log.V(9).Info("ClusterRoleBinding matches.")
-			return nil
-		}
-
-		current.RoleRef = wantRoleBinding.RoleRef
-		current.Subjects = wantRoleBinding.Subjects
-
-		return k8sClient.Update(context.TODO(), current)
+	wantRoleBinding := generator()
+	crb := runtime.NewClusterRoleBinding(name, rbacv1.RoleRef{})
+	op, err := controllerutil.CreateOrUpdate(context.TODO(), k8sClient, crb, func() error {
+		// Update the clusterrolebinding with our desired state
+		crb.RoleRef = wantRoleBinding.RoleRef
+		crb.Subjects = wantRoleBinding.Subjects
+		return nil
 	})
-	return retryErr
+
+	if err == nil {
+		log.V(3).Info(fmt.Sprintf("reconciled clusterRoleBinding - operation: %s", op))
+	}
+	return err
 }
 
 func DeleteClusterRole(k8sClient client.Client, name string) error {
--- internal/reconcile/service_account.go
+++ internal/reconcile/service_account.go
@@ -5,43 +5,22 @@ import (
 	"fmt"
 
 	log "github.com/ViaQ/logerr/v2/log/static"
-	"github.com/openshift/cluster-logging-operator/internal/utils"
+	"github.com/openshift/cluster-logging-operator/internal/runtime"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/client-go/util/retry"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 func ServiceAccount(k8Client client.Client, desired *v1.ServiceAccount) (*v1.ServiceAccount, error) {
-	current := &v1.ServiceAccount{}
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		key := client.ObjectKeyFromObject(desired)
-		if err := k8Client.Get(context.TODO(), key, current); err != nil {
-			if errors.IsNotFound(err) {
-				current = desired
-				return k8Client.Create(context.TODO(), desired)
-			}
-			return fmt.Errorf("failed to get %v ServiceAccount: %w", key, err)
-		}
-
-		same := false
-		if same = (utils.AreMapsSame(current.Annotations, desired.Annotations) &&
-			utils.HasSameOwner(current.OwnerReferences, desired.OwnerReferences)); same {
-			log.V(3).Info("ServiceAccount are the same skipping update")
-			return nil
-		}
-		if current.Annotations == nil {
-			current.Annotations = map[string]string{}
-		}
-		if desired.Annotations != nil {
-			for key, value := range desired.Annotations {
-				current.Annotations[key] = value
-			}
-		}
-
-		current.OwnerReferences = desired.OwnerReferences
-
-		return k8Client.Update(context.TODO(), current)
+	sa := runtime.NewServiceAccount(desired.Namespace, desired.Name)
+	op, err := controllerutil.CreateOrUpdate(context.TODO(), k8Client, sa, func() error {
+		sa.Annotations = desired.Annotations
+		sa.OwnerReferences = desired.OwnerReferences
+		return nil
 	})
-	return current, retryErr
+
+	if err == nil {
+		log.V(3).Info(fmt.Sprintf("reconciled serviceAccount - operation: %s", op))
+	}
+	return sa, err
 }

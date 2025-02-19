d91fabd8692b54bfa7327e04bd00a4e7d489aee9
Casey Hartman <cahartma@redhat.com>
2025-02-19 14:04:45 -0500

    LOG-6755: Add timestamp field for console fix

--- internal/generator/vector/filter/openshift/viaq/common.go
+++ internal/generator/vector/filter/openshift/viaq/common.go
@@ -2,7 +2,7 @@ package viaq
 
 const (
 	ClusterID            = `.openshift.cluster_id = "${OPENSHIFT_CLUSTER_ID:-}"`
-	FixTimestampField    = `ts = del(.timestamp); if !exists(."@timestamp") {."@timestamp" = ts}`
+	FixTimestampField    = `if !exists(."@timestamp") {."@timestamp" = .timestamp}`
 	InternalContext      = `._internal.message = .message`
 	VRLOpenShiftSequence = `.openshift.sequence = to_unix_timestamp(now(), unit: "nanoseconds")`
 )

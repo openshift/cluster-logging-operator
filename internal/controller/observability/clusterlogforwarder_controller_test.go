e4e171d61262fd1bcd1d0ec5e8f43ba013ad4640
Jeff Cantrill <jcantril@redhat.com>
2025-02-24 14:02:10 -0500

    LOG-6758: fix invalid forwarder for lokiStack

---
+++ internal/controller/observability/clusterlogforwarder_controller_test.go
@@ -0,0 +1,128 @@
+package observability
+
+import (
+	"context"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	obs "github.com/openshift/cluster-logging-operator/api/observability/v1"
+	obscontext "github.com/openshift/cluster-logging-operator/internal/api/context"
+	"github.com/openshift/cluster-logging-operator/internal/auth"
+	"github.com/openshift/cluster-logging-operator/internal/runtime"
+	"github.com/openshift/cluster-logging-operator/internal/utils/json"
+	"github.com/openshift/cluster-logging-operator/test"
+	. "github.com/openshift/cluster-logging-operator/test/matchers"
+	authv1 "k8s.io/api/authorization/v1"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/client/interceptor"
+)
+
+var _ = Describe("#Reconcile", func() {
+	Context("LOG-6758", func() {
+		It("should pass validation when the spec is valid", func() {
+			clfYaml := `
+apiVersion: observability.openshift.io/v1
+kind: ClusterLogForwarder
+metadata:
+  name: collector
+  namespace: openshift-logging
+spec:
+  managementState: Managed
+  outputs:
+    - lokiStack:
+        authentication:
+          token:
+            from: serviceAccount
+        target:
+          name: logging-loki
+          namespace: openshift-logging
+      name: default-lokistack
+      tls:
+        ca:
+          configMapName: openshift-service-ca.crt
+          key: service-ca.crt
+      type: lokiStack
+  pipelines:
+    - inputRefs:
+        - audit
+      name: syslog
+      outputRefs:
+        - default-lokistack
+    - inputRefs:
+        - infrastructure
+        - application
+      name: logging-loki
+      outputRefs:
+        - default-lokistack
+    - inputRefs:
+        - application
+      name: container-logs
+      outputRefs:
+        - default-lokistack
+  serviceAccount:
+    name: collector
+`
+			clf := obs.ClusterLogForwarder{}
+			test.MustUnmarshal(clfYaml, &clf)
+			sub := v1.Subject{Kind: "ServiceAccount", Name: clf.Spec.ServiceAccount.Name, Namespace: clf.Namespace}
+			cm := runtime.NewConfigMap(clf.Namespace, "openshift-service-ca.crt", map[string]string{
+				"service-ca.crt": "--service-ca--",
+			})
+			updateStatus := func(obj client.Object) error {
+				aCLF, ok := obj.(*obs.ClusterLogForwarder)
+				if !ok {
+					return nil
+				}
+				clf.Status = aCLF.Status
+				return nil
+			}
+			fakeClient := fake.NewClientBuilder().WithRuntimeObjects(
+				&clf,
+				runtime.NewClusterRoleBinding("collect-app-logs", v1.RoleRef{Name: "collect-application-logs"}, sub),
+				runtime.NewClusterRoleBinding("collect-infra-logs", v1.RoleRef{Name: "collect-infrastructure-logs"}, sub),
+				runtime.NewClusterRoleBinding("collect-audit-logs", v1.RoleRef{Name: "collect-audit-logs"}, sub),
+				runtime.NewServiceAccount(clf.Namespace, clf.Spec.ServiceAccount.Name),
+				auth.NewSCC(),
+				cm,
+			).
+				WithInterceptorFuncs(interceptor.Funcs{
+					Create: func(ctx context.Context, client client.WithWatch, obj client.Object, opts ...client.CreateOption) error {
+						sar, ok := obj.(*authv1.SubjectAccessReview)
+						if !ok {
+							return nil
+						}
+						sar.Status.Allowed = true
+						return nil
+					},
+					SubResourceUpdate: func(ctx context.Context, client client.Client, subResourceName string, obj client.Object, opts ...client.SubResourceUpdateOption) error {
+						return updateStatus(obj)
+					},
+					SubResourcePatch: func(ctx context.Context, client client.Client, subResourceName string, obj client.Object, patch client.Patch, opts ...client.SubResourcePatchOption) error {
+						return updateStatus(obj)
+					},
+				}).
+				Build()
+			fContext := obscontext.ForwarderContext{
+				Client:    fakeClient,
+				Reader:    fakeClient,
+				Forwarder: &clf,
+				ConfigMaps: map[string]*corev1.ConfigMap{
+					cm.Name: cm,
+				},
+			}
+			r := ClusterLogForwarderReconciler{
+				ForwarderContext: fContext,
+			}
+			_, err := r.Reconcile(context.TODO(), ctrl.Request{
+				NamespacedName: types.NamespacedName{Namespace: clf.Namespace, Name: clf.Name},
+			})
+			Expect(err).To(Succeed())
+			Expect(fContext.Forwarder.Status.Conditions).To(HaveCondition(obs.ConditionTypeReady, true, "", ""), json.MustMarshal(fContext.Forwarder.Status.Conditions))
+		})
+	})
+
+})
--- internal/pkg/generator/forwarder/generator.go
+++ internal/pkg/generator/forwarder/generator.go
@@ -4,9 +4,11 @@ import (
 	"errors"
 	"fmt"
 	obs "github.com/openshift/cluster-logging-operator/api/observability/v1"
+	"github.com/openshift/cluster-logging-operator/internal/api/initialize"
 	"github.com/openshift/cluster-logging-operator/internal/factory"
 	forwardergenerator "github.com/openshift/cluster-logging-operator/internal/generator/forwarder"
 	"github.com/openshift/cluster-logging-operator/internal/generator/framework"
+	"github.com/openshift/cluster-logging-operator/internal/utils"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/yaml"
 
@@ -16,15 +18,15 @@ import (
 	"github.com/openshift/cluster-logging-operator/internal/tls"
 )
 
-func UnMarshalClusterLogForwarder(clfYaml string) (forwarder *obs.ClusterLogForwarder, err error) {
-	forwarder = &obs.ClusterLogForwarder{}
+func UnMarshalClusterLogForwarder(clfYaml string) (obs.ClusterLogForwarder, error) {
+	forwarder := &obs.ClusterLogForwarder{}
 	if clfYaml != "" {
-		err = yaml.Unmarshal([]byte(clfYaml), forwarder)
+		err := yaml.Unmarshal([]byte(clfYaml), forwarder)
 		if err != nil {
-			return nil, fmt.Errorf("Error Unmarshalling %q: %v", clfYaml, err)
+			return *forwarder, fmt.Errorf("Error Unmarshalling %q: %v", clfYaml, err)
 		}
 	}
-	return forwarder, err
+	return *forwarder, nil
 }
 
 func Generate(clfYaml string, debugOutput bool, client client.Client) (string, error) {
@@ -35,25 +37,11 @@ func Generate(clfYaml string, debugOutput bool, client client.Client) (string, e
 	}
 	log.V(2).Info("Unmarshalled", "forwarder", forwarder)
 
-	//clRequest := &k8shandler.ClusterLoggingRequest{
-	//	Forwarder: forwarder,
-	//	Cluster: &logging.ClusterLogging{
-	//		ObjectMeta: metav1.ObjectMeta{
-	//			Namespace: forwarder.GetNamespace(),
-	//		},
-	//		Spec: logging.ClusterLoggingSpec{},
-	//	},
-	//	ResourceNames: factory.ResourceNames(*forwarder),
-	//}
-
 	//if client != nil {
 	//	clRequest.Client = client
 	//}
-	//TODO: Enable migrations?
-	//mSpec, extras, condition := migrations.MigrateClusterLogForwarder(forwarder.Namespace, forwarder.Name, forwarder.Spec, clRequest.Cluster.Spec.LogStore, map[string]bool{}, "", "")
-	//log.V(0).Info("Migrated ClusterLogForwarder", "spec", mSpec, "extras", extras, "condition", condition)
-	// forwarder.Spec = mSpec
-	// Set the output secrets if any
+	forwarder = initialize.ClusterLogForwarder(forwarder, utils.NoOptions)
+	log.V(3).Info("Initialized ClusterLogForwarder", "cr", forwarder)
 	// TODO: enable secrets
 	//secrets := internalobs.FetchSecrets(forwarder.Spec.Outputs, client)
 	secrets := map[string]*corev1.Secret{}
@@ -66,5 +54,5 @@ func Generate(clfYaml string, debugOutput bool, client client.Client) (string, e
 	if configGenerator == nil {
 		return "", errors.New("unsupported collector implementation")
 	}
-	return configGenerator.GenerateConf(secrets, forwarder.Spec, forwarder.Namespace, forwarder.Name, *factory.ResourceNames(*forwarder), op)
+	return configGenerator.GenerateConf(secrets, forwarder.Spec, forwarder.Namespace, forwarder.Name, *factory.ResourceNames(forwarder), op)
 }

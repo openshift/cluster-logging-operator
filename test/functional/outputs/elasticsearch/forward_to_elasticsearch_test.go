66cb7e6fe4f89cd991b535cb7b130e430b3b02c0
Calvin Lee <calee@redhat.com>
2025-04-21 15:02:38 -0400

    LOG-7017: Manual port of LOG-5991 to release-6.2

--- test/functional/outputs/elasticsearch/forward_to_elasticsearch_test.go
+++ test/functional/outputs/elasticsearch/forward_to_elasticsearch_test.go
@@ -2,6 +2,7 @@ package elasticsearch
 
 import (
 	"sort"
+	"strconv"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -98,6 +99,7 @@ var _ = Describe("[Functional][Outputs][ElasticSearch] Logforwarding to ElasticS
 				output.Elasticsearch.Authentication = &auth
 			})
 
+		defer framework.Cleanup()
 		Expect(framework.Deploy()).To(BeNil())
 
 		Expect(framework.WritesApplicationLogs(1)).To(BeNil())
@@ -130,8 +132,8 @@ var _ = Describe("[Functional][Outputs][ElasticSearch] Logforwarding to ElasticS
 						Compression: compression,
 					}
 				})
+			defer framework.Cleanup()
 			Expect(framework.Deploy()).To(BeNil())
-
 			// Write app logs
 			timestamp := "2020-11-04T18:13:59.061892+00:00"
 			applicationLogLine := functional.NewCRIOLogMessage(timestamp, "This is my test message", false)
@@ -155,6 +157,8 @@ var _ = Describe("[Functional][Outputs][ElasticSearch] Logforwarding to ElasticS
 			ToElasticSearchOutput(func(output *obs.OutputSpec) {
 				output.Elasticsearch.Index = index
 			})
+		defer framework.Cleanup()
+
 		Expect(framework.Deploy()).To(BeNil())
 
 		// Write app logs
@@ -171,4 +175,121 @@ var _ = Describe("[Functional][Outputs][ElasticSearch] Logforwarding to ElasticS
 		Entry("should write to defined static + dynamic index", `foo-{.log_type||"none"}`, "foo-application"),
 		Entry("should write to defined static + fallback value if field is missing", `foo-{.missing||"none"}`, "foo-none"),
 	)
+
+	Context("elasticsearch authentication", func() {
+		AfterEach(func() {
+			framework.Cleanup()
+		})
+
+		It("should authenticate with username and password", func() {
+			password := "4xpXpbq&rmPCF576N$Bz"
+			secret := runtime.NewSecret("", "mysecret", map[string][]byte{
+				constants.ClientUsername: []byte(functional.ElasticUsername),
+				constants.ClientPassword: []byte(password),
+			})
+
+			outputLogTemplate.ViaqIndexName = "app-write"
+			framework = functional.NewCollectorFunctionalFramework()
+			framework.Secrets = append(framework.Secrets, secret)
+
+			obstestruntime.NewClusterLogForwarderBuilder(framework.Forwarder).
+				FromInput(obs.InputTypeApplication).
+				ToElasticSearchOutput(func(output *obs.OutputSpec) {
+					output.Elasticsearch.Authentication = &obs.HTTPAuthentication{
+						Username: &obs.SecretReference{
+							Key:        constants.ClientUsername,
+							SecretName: "mysecret",
+						},
+						Password: &obs.SecretReference{
+							Key:        constants.ClientPassword,
+							SecretName: "mysecret",
+						},
+					}
+					output.Elasticsearch.Index = `{.log_type||"none"}`
+				})
+
+			esVisitor := func(b *runtime.PodBuilder) error {
+				return framework.AddESOutputWithBasicSecurity(password, b, framework.Forwarder.Spec.Outputs[0])
+			}
+
+			Expect(framework.DeployWithVisitor(esVisitor)).To(BeNil())
+
+			Expect(framework.WritesApplicationLogs(1)).To(BeNil())
+			raw, err := framework.GetLogsFromElasticSearchIndex(string(obs.OutputTypeElasticsearch), string(obs.InputTypeApplication),
+				functional.Option{Name: "username", Value: functional.ElasticUsername},
+				functional.Option{Name: "password", Value: password})
+
+			Expect(err).To(BeNil(), "Expected no errors reading the logs")
+			Expect(raw).To(Not(BeEmpty()))
+
+			// Parse log line
+			var logs []types.ApplicationLog
+			err = types.StrictlyParseLogsFromSlice(raw, &logs)
+			Expect(err).To(BeNil(), "Expected no errors parsing the logs")
+			// Compare to expected template
+			outputTestLog := logs[0]
+			outputLogTemplate.ViaqIndexName = ""
+			Expect(outputTestLog).To(matchers.FitLogFormatTemplate(outputLogTemplate))
+		})
+
+		It("should authenticate with custom bearer token", func() {
+			secret := runtime.NewSecret("", "mysecret", map[string][]byte{
+				constants.ClientUsername: []byte(functional.ElasticUsername),
+				constants.ClientPassword: []byte(functional.ElasticPassword),
+			})
+
+			outputLogTemplate.ViaqIndexName = "app-write"
+			framework = functional.NewCollectorFunctionalFramework()
+
+			obstestruntime.NewClusterLogForwarderBuilder(framework.Forwarder).
+				FromInput(obs.InputTypeApplication).
+				ToElasticSearchOutput(func(output *obs.OutputSpec) {
+					output.Elasticsearch.URL = "http://elasticsearch:9200"
+					output.Elasticsearch.Authentication = &obs.HTTPAuthentication{
+						Token: &obs.BearerToken{
+							From: obs.BearerTokenFromSecret,
+							Secret: &obs.BearerTokenSecretKey{
+								Name: secret.Name,
+								Key:  constants.TokenKey,
+							},
+						},
+					}
+					output.Elasticsearch.Index = `{.log_type||"none"}`
+				})
+
+			// 1. Deploy Pod with ES container with token service enabled along with service
+			Expect(framework.DeployESTokenPodWithService()).To(BeNil())
+
+			// 2. Generate and get token from ES pod
+			token, err := framework.GenerateESAccessToken(string(obs.OutputTypeElasticsearch))
+			Expect(err).To(BeNil())
+			Expect(token).ToNot(BeEmpty())
+
+			// 3. Add token to secret
+			cleanToken, err := strconv.Unquote(token)
+			Expect(err).To(BeNil())
+
+			secret.Data[constants.TokenKey] = []byte(cleanToken)
+			framework.Secrets = append(framework.Secrets, secret)
+
+			// 4. Deploy collector
+			Expect(framework.DeployWithVisitor(nil)).To(BeNil())
+
+			Expect(framework.WritesApplicationLogs(1)).To(BeNil())
+			raw, err := framework.GetLogsFromElasticSearchIndex(string(obs.OutputTypeElasticsearch), string(obs.InputTypeApplication),
+				functional.Option{Name: constants.TokenKey, Value: cleanToken})
+
+			Expect(err).To(BeNil(), "Expected no errors reading the logs")
+			Expect(raw).To(Not(BeEmpty()))
+
+			// Parse log line
+			var logs []types.ApplicationLog
+			err = types.StrictlyParseLogsFromSlice(raw, &logs)
+			Expect(err).To(BeNil(), "Expected no errors parsing the logs")
+			// Compare to expected template
+			outputTestLog := logs[0]
+			outputLogTemplate.ViaqIndexName = ""
+			Expect(outputTestLog).To(matchers.FitLogFormatTemplate(outputLogTemplate))
+		})
+	})
 })

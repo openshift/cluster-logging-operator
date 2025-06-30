21bb49056916aadbac225afb2ed32781f5c0f32e
Vitalii Parfonov <vparfono@redhat.com>
2025-06-30 19:44:38 +0300

    LOG-7058: Add liveness probe to Vector collector container
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- internal/generator/vector/conf/global_test.go
+++ internal/generator/vector/conf/global_test.go
@@ -18,6 +18,7 @@ data_dir = "/var/lib/vector/openshift-logging/my-forwarder"
 
 [api]
 enabled = true
+address = "0.0.0.0:24686"
 
 # Load sensitive data from files
 [secret.kubernetes_secret]
@@ -34,6 +35,7 @@ expire_metrics_secs = 60
 
 [api]
 enabled = true
+address = "0.0.0.0:24686"
 
 # Load sensitive data from files
 [secret.kubernetes_secret]

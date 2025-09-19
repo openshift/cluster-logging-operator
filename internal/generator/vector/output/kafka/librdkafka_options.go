72f81cfa7caa8a657094a238c2007438a0251a8d
Jeff Cantrill <jcantril@redhat.com>
2025-09-19 12:02:22 -0400

    LOG-7609: fix kafka tuning for MaxWrite to solve MessageSizeTooLarge

--- internal/generator/vector/output/kafka/librdkafka_options.go
+++ internal/generator/vector/output/kafka/librdkafka_options.go
@@ -1,16 +1,50 @@
 package kafka
 
-type InsecureTLS struct {
-	ComponentID string
+import (
+	obs "github.com/openshift/cluster-logging-operator/api/observability/v1"
+	"github.com/openshift/cluster-logging-operator/internal/generator/framework"
+	"github.com/openshift/cluster-logging-operator/internal/generator/helpers"
+)
+
+type libRDKafkaOptions struct {
+	ComponentID               string
+	EnableSSLCertVerification helpers.OptionalPair
+	MessageMaxBytes           helpers.OptionalPair
 }
 
-func (i InsecureTLS) Name() string {
-	return "kafkaInsecureTLSTemplate"
+func (i libRDKafkaOptions) isEmpty() bool {
+	return (i.EnableSSLCertVerification.Value == nil ||
+		i.EnableSSLCertVerification.Value == true) &&
+		i.MessageMaxBytes.Value == nil
 }
 
-func (i InsecureTLS) Template() string {
+func (i libRDKafkaOptions) Name() string {
+	return "kafkaLibRDKafkaOptionsTemplate"
+}
+
+func (i libRDKafkaOptions) Template() string {
+	if i.isEmpty() {
+		return `{{define "` + i.Name() + `" -}}{{end}}`
+	}
 	return `{{define "` + i.Name() + `" -}}
 [sinks.{{.ComponentID}}.librdkafka_options]
-"enable.ssl.certificate.verification" = "false"
+{{.EnableSSLCertVerification}}
+{{.MessageMaxBytes}}
 {{- end}}`
+}
+
+func newLibRDKafkaOptions(id string, o obs.OutputSpec, tuningSpec *obs.KafkaTuningSpec) *libRDKafkaOptions {
+	formatter := framework.Option{Name: helpers.OptionFormatter, Value: `%q = "%v"`}
+	libOptions := &libRDKafkaOptions{
+		ComponentID:               id,
+		EnableSSLCertVerification: helpers.NewOptionalPair("enable.ssl.certificate.verification", nil, formatter),
+		MessageMaxBytes:           helpers.NewOptionalPair("message.max.bytes", nil, formatter),
+	}
+	if o.TLS != nil && isTlsBrokers(o) && o.TLS.InsecureSkipVerify {
+		libOptions.EnableSSLCertVerification.Value = false
+	}
+	if tuningSpec != nil && tuningSpec.MaxWrite != nil {
+		libOptions.MessageMaxBytes.Value = tuningSpec.MaxWrite.Value()
+	}
+	return libOptions
 }

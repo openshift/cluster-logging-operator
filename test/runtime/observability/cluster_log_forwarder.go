a75e649f93bd111d5f12b17cedb9bd7a3de21a85
Casey Hartman <cahartma@redhat.com>
2025-10-17 15:21:19 -0400

    adding s3 tests and resolve resulting cw issues

--- test/runtime/observability/cluster_log_forwarder.go
+++ test/runtime/observability/cluster_log_forwarder.go
@@ -186,6 +186,26 @@ func (p *PipelineBuilder) ToCloudwatchOutput(auth obs.AwsAuthentication, visitor
 	return p.ToOutputWithVisitor(v, string(obs.OutputTypeCloudwatch))
 }
 
+// TODO: resolve tests given new aws-sdk requires cw updates as well
+func (p *PipelineBuilder) ToS3Output(auth obs.AwsAuthentication, bucketName, keyPrefix, port string, visitors ...func(output *obs.OutputSpec)) *ClusterLogForwarderBuilder {
+	v := func(output *obs.OutputSpec) {
+		output.Name = string(obs.OutputTypeS3)
+		output.Type = obs.OutputTypeS3
+		output.TLS = &obs.OutputTLSSpec{InsecureSkipVerify: true}
+		output.S3 = &obs.S3{
+			URL:            "https://localhost:" + string(port),
+			Region:         "us-east-2",
+			Bucket:         bucketName,
+			KeyPrefix:      keyPrefix,
+			Authentication: &auth,
+		}
+		for _, v := range visitors {
+			v(output)
+		}
+	}
+	return p.ToOutputWithVisitor(v, string(obs.OutputTypeS3))
+}
+
 func (p *PipelineBuilder) ToLokiOutput(lokiURL url.URL, visitors ...func(output *obs.OutputSpec)) *ClusterLogForwarderBuilder {
 	v := func(output *obs.OutputSpec) {
 		output.Name = string(obs.OutputTypeLoki)

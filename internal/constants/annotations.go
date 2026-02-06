fd740cc33c2299dccff3d56070dfe7b3de391723
Casey Hartman <cahartma@redhat.com>
2026-02-06 15:57:07 -0500

    LOG-8578: remove tech-preview annotation requirement with otel logs

--- internal/constants/annotations.go
+++ internal/constants/annotations.go
@@ -13,9 +13,6 @@ const (
 	// CLO's default log level for vector is `warn`: https://issues.redhat.com/browse/LOG-3435
 	AnnotationVectorLogLevel = "observability.openshift.io/log-level"
 
-	// AnnotationOtlpOutputTechPreview is used to enable tech-preview of OTLP output and LokiStack with Otel data model
-	AnnotationOtlpOutputTechPreview = "observability.openshift.io/tech-preview-otlp-output"
-
 	AnnotationSecretHash = "observability.openshift.io/secret-hash"
 
 	// AnnotationMaxUnavailable (Deprecated) configures the maximum number of DaemonSet pods that can be unavailable during a rolling update.

fc1b75234a244f947d8383d2acb8d5755a150d2a
Vitalii Parfonov <vparfono@redhat.com>
2026-09-08 14:12:40 +0300

    feat(must-gather): refactor impl to golang to remove oc dependency
    bump golang version to 1.26.7
    
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

---
+++ must-gather/internal/api/path_test.go
@@ -0,0 +1,34 @@
+package api_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/openshift/cluster-logging-operator/must-gather/internal/api"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var _ = Describe("Path", func() {
+
+	It("#NewPath should return a path of the args", func() {
+		Expect(api.NewPath("foo", "bar").String()).To(Equal("foo/bar"))
+	})
+
+	It("#Add should return the part to the path", func() {
+		Expect(api.NewPath("foo", "bar").Add("xyz").String()).To(Equal("foo/bar/xyz"))
+	})
+
+	Context("#ForResource", func() {
+
+		var (
+			path = api.NewPath("/root")
+		)
+
+		It("should use the group and resource", func() {
+			Expect(path.ForResource(schema.GroupVersionResource{Group: "apps", Version: "v1", Resource: "deployments"}).String()).To(Equal("/root/apps/deployments"))
+		})
+
+		It("should use only the resource when there is no group", func() {
+			Expect(path.ForResource(schema.GroupVersionResource{Group: "", Version: "v1", Resource: "deployments"}).String()).To(Equal("/root/deployments"))
+		})
+	})
+})
